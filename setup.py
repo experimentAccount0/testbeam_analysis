8d90ff127a3c5561a31dfed8c0df4ab0aa0ee6be
DavidLP <pohl@physik.uni-bonn.de>
2015-10-29 11:51:00 +0100

    MAINT: pypi preperations

--- setup.py
+++ setup.py
@@ -1,86 +1,41 @@
 #!/usr/bin/env python
-
-# Building:
-# python setup.py build_ext
-#
-# Installing package from sources:
-# python setup.py install
-# For developers (creating a link to the sources):
-# python setup.py develop
-#
-# Building source distribution:
-# python setup.py sdist
-# The generated source file is needed for installing by a tool like pip (pip install ...).
-
-
-# from distutils.core import setup
-from setuptools import setup
-from setuptools import find_packages
-from distutils.extension import Extension
-from distutils.command.build_ext import build_ext
-from Cython.Build import cythonize
+from setuptools import setup, find_packages, Extension  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 import numpy as np
-import os
+from Cython.Build import cythonize
 
+cpp_extension = cythonize([
+    Extension('testbeam_analysis.hit_clusterizer', ['testbeam_analysis/clusterizer/hit_clusterizer.pyx', 'testbeam_analysis/clusterizer/Clusterizer.cpp', 'testbeam_analysis/clusterizer/Basis.cpp']),
+    Extension('testbeam_analysis.analysis_functions', ['testbeam_analysis/clusterizer/analysis_functions.pyx'])
+])
 
-copt = {'msvc': ['-IpyTestbeamAnalysis/clusterizer/external', '/EHsc']}  # set additional include path and EHsc exception handling for VS
-lopt = {}
+author = 'Christian Bespin, David-Leon Pohl'
+author_email = 'christian.bespin@uni-bonn.de, pohl@physik.uni-bonn.de'
 
-
-class build_ext_opt(build_ext):
-    def initialize_options(self):
-        build_ext.initialize_options(self)
-        self.compiler = 'msvc' if os.name == 'nt' else None  # in Anaconda the libpython package includes the MinGW import libraries and a file (Lib/distutils/distutils.cfg) which sets the default compiler to mingw32. Alternatively try conda remove libpython.
-
-    def build_extensions(self):
-        c = self.compiler.compiler_type
-        if c in copt:
-            for e in self.extensions:
-                e.extra_compile_args = copt[c]
-        if c in lopt:
-            for e in self.extensions:
-                e.extra_link_args = lopt[c]
-        build_ext.build_extensions(self)
-
-
-extensions = [
-    Extension('pyTestbeamAnalysis.hit_clusterizer', ['pyTestbeamAnalysis/clusterizer/hit_clusterizer.pyx', 'pyTestbeamAnalysis/clusterizer/Clusterizer.cpp', 'pyTestbeamAnalysis/clusterizer/Basis.cpp']),
-    Extension('pyTestbeamAnalysis.analysis_functions', ['pyTestbeamAnalysis/clusterizer/analysis_functions.pyx'])
-]
-
+# requirements for core functionality from requirements.txt
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 f = open('VERSION', 'r')
 version = f.readline().strip()
 f.close()
 
-author = 'David-Leon Pohl'
-author_email = 'pohl@physik.uni-bonn.de'
-
 setup(
-    name='pyTestbeamAnalysis',
+    name='testbeam_analysis',
     version=version,
-    description='A simple test beam analysis in Python.',
-    url='https://github.com/SiLab-Bonn/pyTestbeamAnalysis',
+    description='A light weight test beam analysis in Python and C++.',
+    url='https://github.com/SiLab-Bonn/testbeam_analysis',
     license='BSD 3-Clause ("BSD New" or "BSD Simplified") License',
-    long_description='',
+    long_description='A very simple analysis of pixel-sensor data from testbeams. All steps of a full analysis are included in one file in < 1500 lines of Python code. If you you want to do simple straight line fits without a Kalman filter or you want to understand the basics of telescope reconstruction this code might help. If you want to have something fancy to account for thick devices in combination with low energetic beams use e.g. EUTelescope. Depending on the setup a resolution that is only ~ 15% worse can be archieved with this code. For a quick first impression check the example plots in the wiki and run the examples.',
     author=author,
     maintainer=author,
     author_email=author_email,
     maintainer_email=author_email,
-    install_requires=['cython', 'pandas', 'tables', 'scipy', 'matplotlib'],
+    install_requires=install_requires,
     packages=find_packages(),  # exclude=['*.tests', '*.test']),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
     package_data={'': ['*.txt', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
-    ext_modules=cythonize(extensions),
+    ext_modules=cpp_extension,
     include_dirs=[np.get_include()],
-    cmdclass={'build_ext': build_ext_opt},
+    keywords=['testbeam', 'particle', 'reconstruction', 'pixel', 'detector'],
     platforms='any'
 )
-
-try:
-    from pyTestbeamAnalysis.hit_clusterizer import HitClusterizer
-    import pyTestbeamAnalysis.analysis_functions
-    clusterizer = HitClusterizer()
-    print "STATUS: SUCCESS!"
-except Exception, e:
-    print "STATUS: FAILED (%s)" % str(e)
