04bc729cede548f2dada5a64a6755a24bb085aa9
DavidLP <pohl@physik.uni-bonn.de>
2015-11-26 17:58:21 +0100

    REG: works again until track fitting

--- examples/fei4_telescope_example.py
+++ examples/fei4_telescope_example.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
                   ]
 
     # Dimensions
-    pixel_size = (250, 50)  # in um
+    pixel_size = [(250, 50), (250, 50), (250, 50), (250, 50)]  # in um
     z_positions = [0., 1.95, 10.88, 12.83]  # in cm; optional, can be also deduced from data, but usually not with high precision (~ mm)
 
     output_folder = os.path.split(data_files[0])[0]  # define a folder where all output data and plots are stored
@@ -26,12 +26,16 @@ if __name__ == '__main__':
 
     # The following shows a complete test beam analysis by calling the separate function in correct order
 
+    # Cluster hits off all DUTs
+    Pool().map(tba.cluster_hits, data_files)  # find cluster on all DUT data files in parallel on multiple cores
+    plot_utils.plot_cluster_size(cluster_files,
+                                 output_pdf=output_folder + r'/Cluster_Size.pdf')
+
     # Correlate the row / column of each DUT
     tba.correlate_hits(data_files,
                        alignment_file=output_folder + r'/Correlation.h5',
                        fraction=1,
                        event_range=0)
-
     plot_utils.plot_correlations(alignment_file=output_folder + r'/Correlation.h5',
                                  output_pdf=output_folder + r'/Correlations.pdf')
 
@@ -40,48 +44,45 @@ if __name__ == '__main__':
     tba.align_hits(correlation_file=output_folder + r'/Correlation.h5',
                    alignment_file=output_folder + r'/Alignment.h5',
                    output_pdf=output_folder + r'/Alignment.pdf',
-                   fit_offset_cut=(2. / 10., 5.0),
-                   fit_error_cut=(15. / 1000., 30. / 1000.))
-
-    # Cluster hits off all DUTs
-    Pool().map(tba.cluster_hits, data_files)  # find cluster on all DUT data files in parallel on multiple cores
-    plot_utils.plot_cluster_size(cluster_files,
-                                 output_pdf=output_folder + r'/Cluster_Size.pdf')
+                   fit_offset_cut=(200. / 10., 200. / 10.),
+                   fit_error_cut=(4000. / 1000., 1500. / 1000.),
+                   pixel_size=pixel_size,
+                   show_plots=False)
 
     # Correct all DUT hits via alignment information and merge the cluster tables to one tracklets table aligned at the event number
     tba.merge_cluster_data(cluster_files,
                            alignment_file=output_folder + r'/Alignment.h5',
-                           tracklets_file=output_folder + r'/Tracklets.h5')
+                           tracklets_file=output_folder + r'/Tracklets.h5',
+                           pixel_size=pixel_size)
 
-    # Shift position offsets so that median = 0
-    tba.optimize_hit_alignment(output_folder + r'/Tracklets.h5',
-                               output_folder + r'/Alignment.h5')
-
-    # Check alignment of hits in position and time
-    tba.check_hit_alignment(output_folder + r'/Tracklets.h5',
-                            output_folder + r'/Alignment_Check.pdf')
-
-    # Fix event alignment and flag correlated data (not needed for example data)
-    tba.fix_event_alignment(output_folder + r'/Tracklets.h5',
-                            output_folder + r'/Tracklets_corr.h5',
-                            output_folder + r'/Alignment.h5',
-                            error=8.,
-                            n_bad_events=50)
+# Optional: These are additional steps not mandatory for good data (like the data used here)
+#     # Shift position offsets so that median = 0
+#     tba.optimize_hit_alignment(output_folder + r'/Tracklets.h5',
+#                                output_folder + r'/Alignment.h5')
+#     # Check alignment of hits in position and time
+#     tba.check_hit_alignment(output_folder + r'/Tracklets.h5',
+#                             output_folder + r'/Alignment_Check.pdf')
+#
+#     # Fix event alignment and flag correlated data (not needed for example data)
+#     tba.fix_event_alignment(output_folder + r'/Tracklets.h5',
+#                             output_folder + r'/Tracklets_corr.h5',
+#                             output_folder + r'/Alignment.h5',
+#                             error=8.,
+#                             n_bad_events=50)
 
     # Find tracks from the tracklets and stores the with quality indicator into track candidates table
-    tba.find_tracks(tracklets_file=output_folder + r'/Tracklets_corr.h5',
+    tba.find_tracks(tracklets_file=output_folder + r'/Tracklets.h5',
                     alignment_file=output_folder + r'/Alignment.h5',
-                    track_candidates_file=output_folder + r'/TrackCandidates.h5',
-                    pixel_size=pixel_size)
+                    track_candidates_file=output_folder + r'/TrackCandidates.h5')
 
-    # optional: try to deduce the devices z positions. Difficult for parallel tracks / bad resolution and does actually not really help here. Still good for cross check.
-    tba.align_z(track_candidates_file=output_folder + r'/TrackCandidates.h5',
-                alignment_file=output_folder + r'/Alignment.h5',
-                output_pdf=output_folder + r'/Z_positions.pdf',
-                z_positions=z_positions,
-                track_quality=2,
-                max_tracks=1,
-                warn_at=0.5)
+# Optional: Try to deduce the devices z positions. Difficult for parallel tracks / bad resolution and does actually not really help here. Still good for cross check.
+#     tba.align_z(track_candidates_file=output_folder + r'/TrackCandidates.h5',
+#                 alignment_file=output_folder + r'/Alignment.h5',
+#                 output_pdf=output_folder + r'/Z_positions.pdf',
+#                 z_positions=z_positions,
+#                 track_quality=2,
+#                 max_tracks=1,
+#                 warn_at=0.5)
 
     # Fit the track candidates and create new track table
     tba.fit_tracks(track_candidates_file=output_folder + r'/TrackCandidates.h5',
@@ -93,45 +94,44 @@ if __name__ == '__main__':
                    ignore_duts=None,
                    max_tracks=1,
                    track_quality=1,
-                   pixel_size=pixel_size,
                    use_correlated=False)
-
-    # optional: plot some tracks (or track candidates) of a selected event range
-    plot_utils.plot_events(track_file=output_folder + r'/Tracks.h5',
-                           output_pdf=output_folder + r'/Event.pdf',
-                           z_positions=z_positions,
-                           event_range=(0, 10),
-                           pixel_size=pixel_size,
-                           plot_lim=(2, 2),
-                           dut=1)
-
-    # Calculate the residuals to check the alignment
-    tba.calculate_residuals(tracks_file=output_folder + r'/Tracks.h5',
-                            output_pdf=output_folder + r'/Residuals.pdf',
-                            z_positions=z_positions,
-                            pixel_size=pixel_size,
-                            use_duts=None,
-                            max_chi2=3e3)
-
-    # Plot the track density on selected DUT planes
-    plot_utils.plot_track_density(tracks_file=output_folder + r'/Tracks.h5',
-                                  output_pdf=output_folder + r'/TrackDensity.pdf',
-                                  z_positions=z_positions,
-                                  dim_x=80,
-                                  dim_y=336,
-                                  use_duts=None)
-
-    # Calculate the efficiency and mean hit/track hit distance
-    # When needed, set included column and row range for each DUT as list of tuples
-    tba.calculate_efficiency(tracks_file=output_folder + r'/Tracks.h5',
-                             output_pdf=output_folder + r'/Efficiency.pdf',
-                             z_positions=z_positions,
-                             dim_x=80,
-                             dim_y=336,
-                             minimum_track_density=2,
-                             pixel_size=pixel_size,
-                             use_duts=None,
-                             cut_distance=500,
-                             max_distance=500,
-                             col_range=(5, 70),
-                             row_range=(20, 320))
+#
+#     # optional: plot some tracks (or track candidates) of a selected event range
+#     plot_utils.plot_events(track_file=output_folder + r'/Tracks.h5',
+#                            output_pdf=output_folder + r'/Event.pdf',
+#                            z_positions=z_positions,
+#                            event_range=(0, 10),
+#                            pixel_size=pixel_size,
+#                            plot_lim=(2, 2),
+#                            dut=1)
+#
+#     # Calculate the residuals to check the alignment
+#     tba.calculate_residuals(tracks_file=output_folder + r'/Tracks.h5',
+#                             output_pdf=output_folder + r'/Residuals.pdf',
+#                             z_positions=z_positions,
+#                             pixel_size=pixel_size,
+#                             use_duts=None,
+#                             max_chi2=3e3)
+#
+#     # Plot the track density on selected DUT planes
+#     plot_utils.plot_track_density(tracks_file=output_folder + r'/Tracks.h5',
+#                                   output_pdf=output_folder + r'/TrackDensity.pdf',
+#                                   z_positions=z_positions,
+#                                   dim_x=80,
+#                                   dim_y=336,
+#                                   use_duts=None)
+#
+#     # Calculate the efficiency and mean hit/track hit distance
+#     # When needed, set included column and row range for each DUT as list of tuples
+#     tba.calculate_efficiency(tracks_file=output_folder + r'/Tracks.h5',
+#                              output_pdf=output_folder + r'/Efficiency.pdf',
+#                              z_positions=z_positions,
+#                              dim_x=80,
+#                              dim_y=336,
+#                              minimum_track_density=2,
+#                              pixel_size=pixel_size,
+#                              use_duts=None,
+#                              cut_distance=500,
+#                              max_distance=500,
+#                              col_range=(5, 70),
+#                              row_range=(20, 320))
