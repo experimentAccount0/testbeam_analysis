6459f33a46c77d4a2af4fbf563ea57ac5770d05d
ChristianBesp <christian.bespin@gmail.com>
2016-01-26 16:14:40 +0100

    MAINT: make it work again

--- tests/test_result_analysis.py
+++ tests/test_result_analysis.py
@@ -1,8 +1,6 @@
 ''' Script to check the correctness of the analysis. The analysis is done on raw data and all results are compared to a recorded analysis.
 '''
 import unittest
-import tables as tb
-import numpy as np
 import os
 
 from testbeam_analysis import result_analysis
@@ -21,46 +19,41 @@ class TestResultAnalysis(unittest.TestCase):
             cls.vdisplay.start()
         cls.output_folder = tests_data_folder
         cls.pixel_size = (250, 50)  # in um
-        cls.z_positions = [0., 1.95, 10.88, 12.83]  # in cm
+        cls.z_positions = [0., 19500, 108800, 128300]  # in um
 
     @classmethod
     def tearDownClass(cls):  # remove created files
-        pass
-#         os.remove(cls.output_folder + 'Efficiency.pdf')
+        os.remove(cls.output_folder + 'Efficiency.pdf')
 #         os.remove(cls.output_folder + 'Residuals.pdf')
 
-    @unittest.SkipTest
     def test_residuals_calculation(self):
         residuals = result_analysis.calculate_residuals(tracks_file=tests_data_folder + 'Tracks_result.h5',
                                                         output_pdf=self.output_folder + 'Residuals.pdf',
                                                         z_positions=self.z_positions,
                                                         use_duts=None,
-                                                        max_chi2=3e3)
+                                                        max_chi2=10000)
 
-        # Only test row residuals, columns are to large (250 um) for meaningfull gaussian residuals distribution
-        self.assertAlmostEqual(residuals[1], 31.012493732525773, msg='DUT 0 row residuals do not match')
-        self.assertAlmostEqual(residuals[3], 21.480965949719472, msg='DUT 1 row residuals do not match')
-        self.assertAlmostEqual(residuals[5], 31.372070715636198, msg='DUT 2 row residuals do not match')
-        self.assertAlmostEqual(residuals[7], 44.449772465251371, msg='DUT 3 row residuals do not match')
+        # Only test row residuals, columns are too large (250 um) for meaningfull gaussian residuals distribution
+        self.assertAlmostEqual(residuals[1], 22.913569705886552, msg='DUT 0 row residuals do not match')
+        self.assertAlmostEqual(residuals[3], 18.73170503500376, msg='DUT 1 row residuals do not match')
+        self.assertAlmostEqual(residuals[5], 22.864586411335129, msg='DUT 2 row residuals do not match')
+        self.assertAlmostEqual(residuals[7], 27.28165821984965, msg='DUT 3 row residuals do not match')
 
-    @unittest.SkipTest
     def test_efficiency_calculation(self):
         efficiencies = result_analysis.calculate_efficiency(tracks_file=self.output_folder + 'Tracks_result.h5',
-                                                            output_pdf=self.output_folder + 'Efficiency.pdf',
+                                                            output_pdf=self.output_folder + r'/Efficiency.pdf',
                                                             z_positions=self.z_positions,
-                                                            dim_x=80,
-                                                            dim_y=336,
+                                                            bin_size=(250, 50),
                                                             minimum_track_density=2,
-                                                            pixel_size=self.pixel_size,
                                                             use_duts=None,
                                                             cut_distance=500,
                                                             max_distance=500,
-                                                            col_range=(5, 70),
-                                                            row_range=(20, 320))
-        self.assertAlmostEqual(efficiencies[0], 100, msg='DUT 0 efficiencies do not match')
-        self.assertAlmostEqual(efficiencies[1], 98.113207547169807, msg='DUT 1 efficiencies do not match')
-        self.assertAlmostEqual(efficiencies[2], 97.484276729559738, msg='DUT 2 efficiencies do not match')
-        self.assertAlmostEqual(efficiencies[3], 92.666666666666671, msg='DUT 3 efficiencies do not match')
+                                                            col_range=(1250, 17500),
+                                                            row_range=(1000, 16000))
+        self.assertAlmostEqual(efficiencies[0], 100.0, msg='DUT 0 efficiencies do not match')
+        self.assertAlmostEqual(efficiencies[1], 98.701298701298697, msg='DUT 1 efficiencies do not match')
+        self.assertAlmostEqual(efficiencies[2], 97.468354430379748, msg='DUT 2 efficiencies do not match')
+        self.assertAlmostEqual(efficiencies[3], 100.0, msg='DUT 3 efficiencies do not match')
 
 if __name__ == '__main__':
     tests_data_folder = r'test_result_analysis/'
