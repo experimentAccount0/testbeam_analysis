8c4065fc94cd5d6309d52a1fffaf0f082a9404e4
DavidLP <david-leon.pohl@rub.de>
2016-01-26 19:23:37 +0100

    ENH: decrease the result comparison

--- tests/test_result_analysis.py
+++ tests/test_result_analysis.py
@@ -32,12 +32,11 @@ class TestResultAnalysis(unittest.TestCase):
                                                         z_positions=self.z_positions,
                                                         use_duts=None,
                                                         max_chi2=10000)
-
         # Only test row residuals, columns are too large (250 um) for meaningfull gaussian residuals distribution
-        self.assertAlmostEqual(residuals[1], 22.913569705886552, msg='DUT 0 row residuals do not match')
-        self.assertAlmostEqual(residuals[3], 18.73170503500376, msg='DUT 1 row residuals do not match')
-        self.assertAlmostEqual(residuals[5], 22.864586411335129, msg='DUT 2 row residuals do not match')
-        self.assertAlmostEqual(residuals[7], 27.28165821984965, msg='DUT 3 row residuals do not match')
+        self.assertAlmostEqual(residuals[1], 22.9135, msg='DUT 0 row residuals do not match', places=3)
+        self.assertAlmostEqual(residuals[3], 18.7317, msg='DUT 1 row residuals do not match', places=3)
+        self.assertAlmostEqual(residuals[5], 22.8645, msg='DUT 2 row residuals do not match', places=3)
+        self.assertAlmostEqual(residuals[7], 27.2816, msg='DUT 3 row residuals do not match', places=3)
 
     def test_efficiency_calculation(self):
         efficiencies = result_analysis.calculate_efficiency(tracks_file=self.output_folder + 'Tracks_result.h5',
@@ -50,10 +49,10 @@ class TestResultAnalysis(unittest.TestCase):
                                                             max_distance=500,
                                                             col_range=(1250, 17500),
                                                             row_range=(1000, 16000))
-        self.assertAlmostEqual(efficiencies[0], 100.0, msg='DUT 0 efficiencies do not match')
-        self.assertAlmostEqual(efficiencies[1], 98.701298701298697, msg='DUT 1 efficiencies do not match')
-        self.assertAlmostEqual(efficiencies[2], 97.468354430379748, msg='DUT 2 efficiencies do not match')
-        self.assertAlmostEqual(efficiencies[3], 100.0, msg='DUT 3 efficiencies do not match')
+        self.assertAlmostEqual(efficiencies[0], 100.0, msg='DUT 0 efficiencies do not match', places=3)
+        self.assertAlmostEqual(efficiencies[1], 98.701298701298697, msg='DUT 1 efficiencies do not match', places=3)
+        self.assertAlmostEqual(efficiencies[2], 97.468354430379748, msg='DUT 2 efficiencies do not match', places=3)
+        self.assertAlmostEqual(efficiencies[3], 100.0, msg='DUT 3 efficiencies do not match', places=3)
 
 if __name__ == '__main__':
     tests_data_folder = r'test_result_analysis/'
