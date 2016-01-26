2e62fabfd4228f15be7ba877a538bc5a766c03f7
DavidLP <david-leon.pohl@rub.de>
2016-01-26 19:23:19 +0100

    MAINT: use toot test module for array/h5 file comparisons

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -1,108 +1,12 @@
 ''' Script to check the correctness of the analysis. The analysis is done on raw data and all results are compared to a recorded analysis.
 '''
 import unittest
-import tables as tb
-import numpy as np
 import os
 
 from testbeam_analysis import dut_alignment
+from testbeam_analysis.tools import test_tools
 
 tests_data_folder = r'tests/test_dut_alignment/'
-
-
-def get_array_differences(first_array, second_array):
-    '''Takes two numpy.ndarrays and compares them on a column basis.
-    Different column data types, missing columns and columns with different values are returned in a string.
-
-    Parameters
-    ----------
-    first_array : numpy.ndarray
-    second_array : numpy.ndarray
-
-    Returns
-    -------
-    string
-    '''
-    if first_array.dtype.names is None:  # normal nd.array
-        return ': Sum first array: ' + str(np.sum(first_array)) + ', Sum second array: ' + str(np.sum(second_array))
-    else:
-        return_str = ''
-        for column_name in first_array.dtype.names:
-            first_column = first_array[column_name]
-            try:
-                second_column = second_array[column_name]
-            except ValueError:
-                return_str += 'No ' + column_name + ' column found. '
-                continue
-            if (first_column.dtype != second_column.dtype):
-                return_str += 'Column ' + column_name + ' has different data type. '
-            try:
-                if not (first_column == second_column).all():  # check if the data of the column is equal
-                    return_str += 'Column ' + column_name + ' not equal. '
-            except AttributeError:
-                if not (first_column == second_column):
-                    return_str += 'Column ' + column_name + ' not equal. '
-        for column_name in second_array.dtype.names:
-            try:
-                first_array[column_name]
-            except ValueError:
-                return_str += 'Additional column ' + column_name + ' found. '
-                continue
-        return ': ' + return_str
-
-
-def compare_h5_files(first_file, second_file, expected_nodes=None, detailed_comparison=True, exact=True):
-    '''Takes two hdf5 files and check for equality of all nodes.
-    Returns true if the node data is equal and the number of nodes is the number of expected nodes.
-    It also returns a error string containing the names of the nodes that are not equal.
-
-    Parameters
-    ----------
-    first_file : string
-        Path to the first file.
-    second_file : string
-        Path to the first file.
-    expected_nodes : Int
-        The number of nodes expected in the second_file. If not specified the number of nodes expected in the second_file equals
-        the number of nodes in the first file.
-
-    Returns
-    -------
-    bool, string
-    '''
-    checks_passed = True
-    error_msg = ""
-    with tb.open_file(first_file, 'r') as first_h5_file:
-        with tb.open_file(second_file, 'r') as second_h5_file:
-            n_expected_nodes = sum(1 for _ in enumerate(first_h5_file.root)) if expected_nodes is None else expected_nodes  # set the number of expected nodes
-            n_nodes = sum(1 for _ in enumerate(second_h5_file.root))  # calculated the number of nodes
-            if n_nodes != n_expected_nodes:
-                checks_passed = False
-                error_msg += 'The number of nodes in the file is wrong.\n'
-            for node in second_h5_file.root:  # loop over all nodes and compare each node, do not abort if one node is wrong
-                node_name = node.name
-                try:
-                    expected_data = first_h5_file.get_node(first_h5_file.root, node_name)[:]
-                    data = second_h5_file.get_node(second_h5_file.root, node_name)[:]
-                    if (exact or expected_data.dtype.names is not None):  # exact comparison if exact is set and on recarray data (np.allclose does not work on recarray)
-                        if not np.array_equal(expected_data, data):  # compare the arrays for each element
-                            checks_passed = False
-                            error_msg += node_name
-                            if detailed_comparison:
-                                error_msg += get_array_differences(expected_data, data)
-                            error_msg += '\n'
-                    else:
-                        if not np.allclose(expected_data, data):
-                            np.allclose(expected_data, data)
-                            checks_passed = False
-                            error_msg += node_name
-                            if detailed_comparison:
-                                error_msg += get_array_differences(expected_data, data)
-                            error_msg += '\n'
-                except tb.NoSuchNodeError:
-                    checks_passed = False
-                    error_msg += 'Unknown node ' + node_name + '\n'
-    return checks_passed, error_msg
 
 
 class TestHitAnalysis(unittest.TestCase):
@@ -133,7 +37,7 @@ class TestHitAnalysis(unittest.TestCase):
                                      alignment_file=self.output_folder + 'Correlation.h5',
                                      fraction=1,
                                      event_range=0)
-        data_equal, error_msg = compare_h5_files(tests_data_folder + 'Correlation_result.h5', self.output_folder + 'Correlation.h5', exact=False)
+        data_equal, error_msg = test_tools.compare_h5_files(tests_data_folder + 'Correlation_result.h5', self.output_folder + 'Correlation.h5', exact=False)
         self.assertTrue(data_equal, msg=error_msg)
 
     def test_hit_alignment(self):  # check the hit alignment function
@@ -142,8 +46,9 @@ class TestHitAnalysis(unittest.TestCase):
                                  output_pdf=self.output_folder + 'Alignment.pdf',
                                  fit_offset_cut=(2000. / 10., 2000. / 10.),
                                  fit_error_cut=(10000. / 1000., 10000. / 1000.),
-                                 pixel_size=self.pixel_size)
-        data_equal, error_msg = compare_h5_files(tests_data_folder + 'Alignment_result.h5', self.output_folder + 'Alignment.h5', exact=False)
+                                 pixel_size=self.pixel_size,
+                                 show_plots=False)
+        data_equal, error_msg = test_tools.compare_h5_files(tests_data_folder + 'Alignment_result.h5', self.output_folder + 'Alignment.h5', exact=False)
         self.assertTrue(data_equal, msg=error_msg)
 
     def test_cluster_merging(self):
@@ -152,7 +57,7 @@ class TestHitAnalysis(unittest.TestCase):
                                          alignment_file=tests_data_folder + 'Alignment_result.h5',
                                          tracklets_file=self.output_folder + 'Tracklets.h5',
                                          pixel_size=self.pixel_size)
-        data_equal, error_msg = compare_h5_files(tests_data_folder + 'Tracklets_result.h5', self.output_folder + 'Tracklets.h5')
+        data_equal, error_msg = test_tools.compare_h5_files(tests_data_folder + 'Tracklets_result.h5', self.output_folder + 'Tracklets.h5')
         self.assertTrue(data_equal, msg=error_msg)
 
 if __name__ == '__main__':
--- tests/test_correlation_fixing.py
+++ tests/test_correlation_fixing.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from testbeam_analysis import analysis_utils
 from testbeam_analysis import analysis_functions
+from testbeam_analysis.tools import test_tools
 
 
 def get_random_data(n_hits, hits_per_event=2, seed=0):
@@ -25,100 +26,6 @@ def get_random_data(n_hits, hits_per_event=2, seed=0):
     charge = np.ascontiguousarray(charge)
 
     return event_numbers, ref_column, column, ref_row, row, ref_charge, charge, corr
-
-
-def get_array_differences(first_array, second_array):
-    '''Takes two numpy.ndarrays and compares them on a column basis.
-    Different column data types, missing columns and columns with different values are returned in a string.
-
-    Parameters
-    ----------
-    first_array : numpy.ndarray
-    second_array : numpy.ndarray
-
-    Returns
-    -------
-    string
-    '''
-    if first_array.dtype.names is None:  # normal nd.array
-        return ': Sum first array: ' + str(np.sum(first_array)) + ', Sum second array: ' + str(np.sum(second_array))
-    else:
-        return_str = ''
-        for column_name in first_array.dtype.names:
-            first_column = first_array[column_name]
-            try:
-                second_column = second_array[column_name]
-            except ValueError:
-                return_str += 'No ' + column_name + ' column found. '
-                continue
-            if (first_column.dtype != second_column.dtype):
-                return_str += 'Column ' + column_name + ' has different data type. '
-            try:
-                if not (first_column == second_column).all():  # check if the data of the column is equal
-                    return_str += 'Column ' + column_name + ' not equal. '
-            except AttributeError:
-                if not (first_column == second_column):
-                    return_str += 'Column ' + column_name + ' not equal. '
-        for column_name in second_array.dtype.names:
-            try:
-                first_array[column_name]
-            except ValueError:
-                return_str += 'Additional column ' + column_name + ' found. '
-                continue
-        return ': ' + return_str
-
-
-def compare_h5_files(first_file, second_file, expected_nodes=None, detailed_comparison=True):
-    '''Takes two hdf5 files and check for equality of all nodes.
-    Returns true if the node data is equal and the number of nodes is the number of expected nodes.
-    It also returns a error string containing the names of the nodes that are not equal.
-
-    Parameters
-    ----------
-    first_file : string
-        Path to the first file.
-    second_file : string
-        Path to the first file.
-    expected_nodes : Int
-        The number of nodes expected in the second_file. If not specified the number of nodes expected in the second_file equals
-        the number of nodes in the first file.
-
-    Returns
-    -------
-    bool, string
-    '''
-    checks_passed = True
-    error_msg = ""
-    with tb.open_file(first_file, 'r') as first_h5_file:
-        with tb.open_file(second_file, 'r') as second_h5_file:
-            expected_nodes = sum(1 for _ in enumerate(first_h5_file.root)) if expected_nodes is None else expected_nodes  # set the number of expected nodes
-            nodes = sum(1 for _ in enumerate(second_h5_file.root))  # calculated the number of nodes
-            if nodes != expected_nodes:
-                checks_passed = False
-                error_msg += 'The number of nodes in the file is wrong.\n'
-            for node in second_h5_file.root:  # loop over all nodes and compare each node, do not abort if one node is wrong
-                node_name = node.name
-                try:
-                    expected_data = first_h5_file.get_node(first_h5_file.root, node_name)[:]
-                    data = second_h5_file.get_node(second_h5_file.root, node_name)[:]
-                    try:
-                        if not (expected_data == data).all():  # compare the arrays for each element
-                            checks_passed = False
-                            error_msg += node_name
-                            if detailed_comparison:
-                                error_msg += get_array_differences(expected_data, data)
-                            error_msg += '\n'
-                    except AttributeError:  # .all() only works on non scalars, recarray is somewhat a scalar
-                        if not (expected_data == data):
-                            checks_passed = False
-                            error_msg += node_name
-                            if detailed_comparison:
-                                error_msg += get_array_differences(expected_data, data)
-                            error_msg += '\n'
-                except tb.NoSuchNodeError:
-                    checks_passed = False
-                    error_msg += 'Unknown node ' + node_name + '\n'
-    return checks_passed, error_msg
 
 
 class TestCorrelationFixing(unittest.TestCase):
