15031e4fbaccdfa1fc8ca0218c402355e9fb1624
DavidLP <david-leon.pohl@rub.de>
2016-02-13 17:51:35 +0100

    MAINT: use new cluster wrappe API

--- examples/fei4_telescope_example.py
+++ examples/fei4_telescope_example.py
@@ -35,7 +35,11 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
     # The following shows a complete test beam analysis by calling the separate function in correct order
 
     # Cluster hits off all DUTs
-    args = [(data_files[i], 1, 2, 2, 1000000) for i in range(0, len(data_files))]
+    args = [{'data_file': data_files[i],
+             'max_x_distance': 2,
+             'max_y_distance': 1,
+             'max_time_distance': 2,
+             'max_cluster_hits':1000} for i in range(0, len(data_files))]
     Pool().map(hit_analysis.cluster_hits_wrapper, args)  # find cluster on all DUT data files in parallel on multiple cores
     plot_utils.plot_cluster_size(cluster_files,
                                  output_pdf=output_folder + r'/Cluster_Size.pdf')
