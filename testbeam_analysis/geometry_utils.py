5f15733250d8d7f24fbfa44ac365b518af4c3173
silab <silab@silab56.physik.uni-bonn.de>
2016-03-14 17:09:13 +0100

    ENH: rotation angles do not use approximations for small angels MAINT: other stuff

--- testbeam_analysis/geometry_utils.py
+++ testbeam_analysis/geometry_utils.py
@@ -7,6 +7,8 @@ from math import sqrt
 import tables as tb
 import numpy as np
 
+from math import sin
+from math import asin
 
 def create_initial_geometry(outFile, zpos, initial_translation=None, initial_rotation=None):
     with tb.open_file(outFile, mode='w') as geoFile:
@@ -129,27 +131,74 @@ def update_rotation_angle(geoFile, dut, val, mode="Absolute", angle="Gamma"):
     nplanes = xy_translation.shape[0]
     tangamma = rotation_matrixes[dut, 0, 1] / rotation_matrixes[dut, 0, 0]
     angles = {}
-    angles["Gamma"] = sqrt(tangamma ** 2 / (1 + tangamma ** 2))
-    angles["Beta"] = -rotation_matrixes[dut, 0, 2]
-    angles["Alpha"] = rotation_matrixes[dut, 1, 2] / sqrt(1 - angles["Beta"] ** 2)
-    '''Not expecting big angles, operating in the approximation sin(theta) ~ theta '''
+    angles["Gamma"] = asin(sqrt(tangamma ** 2 / (1 + tangamma ** 2)))
+    angles["Beta"] = asin(-rotation_matrixes[dut, 0, 2])
+    angles["Alpha"] = asin(rotation_matrixes[dut, 1, 2] / sqrt(1 - angles["Beta"] ** 2))
+
     if mode == "Absolute":
         angles[angle] = val
     elif mode == "Relative":
         angles[angle] += val
+        
+    singamma = sin(angles["Gamma"])
+    sinbeta = sin(angles["Beta"])
+    sinalpha = sin(angles["Alpha"])
 
-    cosgamma = sqrt(1 - angles["Gamma"] ** 2)
-    cosbeta = sqrt(1 - angles["Beta"] ** 2)
-    cosalpha = sqrt(1 - angles["Alpha"] ** 2)
+    cosgamma = sqrt(1 - singamma ** 2)
+    cosbeta = sqrt(1 - sinbeta ** 2)
+    cosalpha = sqrt(1 - sinalpha ** 2)
     ''' Boring application: is there any method to produce rotation matrixes? '''
     rotation_matrixes[dut, 0, 0] = cosbeta * cosgamma
-    rotation_matrixes[dut, 0, 1] = cosbeta * angles["Gamma"]
-    rotation_matrixes[dut, 0, 2] = -angles["Beta"]
-    rotation_matrixes[dut, 1, 0] = (-angles["Gamma"] * cosalpha + angles["Alpha"] * angles["Beta"] * cosgamma)
-    rotation_matrixes[dut, 1, 1] = (cosgamma * cosalpha + angles["Alpha"] * angles["Beta"] * angles["Gamma"])
-    rotation_matrixes[dut, 1, 2] = cosbeta * angles["Alpha"]
-    rotation_matrixes[dut, 2, 0] = cosalpha * angles["Beta"] * cosgamma + angles["Alpha"] * angles["Gamma"]
-    rotation_matrixes[dut, 2, 1] = cosalpha * angles["Beta"] * angles["Gamma"] - angles["Alpha"] * cosgamma
+    rotation_matrixes[dut, 0, 1] = cosbeta * singamma
+    rotation_matrixes[dut, 0, 2] = -sinbeta
+    rotation_matrixes[dut, 1, 0] = (-singamma * cosalpha + sinalpha * sinbeta * cosgamma)
+    rotation_matrixes[dut, 1, 1] = (cosgamma * cosalpha + sinalpha * sinbeta * singamma)
+    rotation_matrixes[dut, 1, 2] = cosbeta * sinalpha
+    rotation_matrixes[dut, 2, 0] = cosalpha * sinbeta * cosgamma + sinalpha * singamma
+    rotation_matrixes[dut, 2, 1] = cosalpha * sinbeta * singamma - sinalpha * cosgamma
+    rotation_matrixes[dut, 2, 2] = cosbeta * cosalpha
+
+    update_geometry(geoFile, dut, xy_translation, rotation_matrixes, nplanes)
+
+
+def update_rotation_angles(geoFile, dut, vals, mode="Absolute"):
+    '''mode:
+            --Absolute: move x and y to that specified values
+            --Relative: increment the acutal x,y values by the specified ones
+        note here we use the rotation matrix as R_tot = R(alpha)R(beta)R(gamma), i.e.
+        R_tot = [cosgamma*cosbeta,                                singamma*cosbeta,                                    -sinbeta]
+                [(-singamma*cosalfa + sinalfa*sinbeta*cosgamma), (cosgamma*cosalfa +sinalfa*sinbeta*singamma), cosbeta*sinalpha]
+                [...]'''
+
+    xy_translation, rotation_matrixes = recontruct_geometry_from_file(geoFile)
+    nplanes = xy_translation.shape[0]
+    tangamma = rotation_matrixes[dut, 0, 1] / rotation_matrixes[dut, 0, 0]
+    angles = np.zeros(3)
+    angles[2] = np.sign(tangamma)*asin(sqrt(tangamma ** 2 / (1 + tangamma ** 2)))
+    angles[1] = asin(-rotation_matrixes[dut, 0, 2])
+    angles[0] = asin(rotation_matrixes[dut, 1, 2] / sqrt(1 - angles[1] ** 2))
+    
+    if mode == "Absolute":
+        angles = vals
+    elif mode == "Relative":
+        angles += vals
+
+    singamma = sin(angles[2])
+    sinbeta = sin(angles[1])
+    sinalpha = sin(angles[0])
+
+    cosgamma = sqrt(1 - singamma ** 2)
+    cosbeta = sqrt(1 - sinbeta ** 2)
+    cosalpha = sqrt(1 - sinalpha ** 2)
+    ''' Boring application: is there any method to produce rotation matrixes? '''
+    rotation_matrixes[dut, 0, 0] = cosbeta * cosgamma
+    rotation_matrixes[dut, 0, 1] = cosbeta * singamma
+    rotation_matrixes[dut, 0, 2] = -sinbeta
+    rotation_matrixes[dut, 1, 0] = (-singamma * cosalpha + sinalpha * sinbeta * cosgamma)
+    rotation_matrixes[dut, 1, 1] = (cosgamma * cosalpha + sinalpha * sinbeta * singamma)
+    rotation_matrixes[dut, 1, 2] = cosbeta * sinalpha
+    rotation_matrixes[dut, 2, 0] = cosalpha * sinbeta * cosgamma + sinalpha * singamma
+    rotation_matrixes[dut, 2, 1] = cosalpha * sinbeta * singamma - sinalpha * cosgamma
     rotation_matrixes[dut, 2, 2] = cosbeta * cosalpha
 
     update_geometry(geoFile, dut, xy_translation, rotation_matrixes, nplanes)
@@ -171,3 +220,34 @@ def update_translation_val(geoFile, dut, xval, yval, mode="Absolute"):
     else:
         print("update_translation_val: mode not recognized. Not applying any translation.")
     update_geometry(geoFile, dut, xy_translation, rotation_matrixes, nplanes)
+    
+def modifiy_alignment (alignment_file):
+    with tb.open_file(alignment_file, mode='r') as alignment:
+        corrs = alignment.root.Alignment[:]
+        
+        for i in range(corrs.shape[0]):
+            if corrs["c1"][i] > 0:
+                corrs["c1"][i] = 1
+            else:
+                corrs["c1"][i] = -1
+            corrs["c1_error"][i] = 50000
+            corrs["sigma"][i] = 500
+            
+    with tb.open_file(alignment_file, mode='w') as alignment:
+        try:
+            result_table = alignment.create_table(alignment.root, name='Alignment', description=corrs.dtype, title='Correlation data', filters=tb.Filters(complib='blosc', complevel=5, fletcher32=False))
+            result_table.append(corrs)
+        except tb.exceptions.NodeError:
+            logging.warning('Correlation table exists already. Do not create new.')
+    
+
+    
+def reset_geometry(geometry, dut):
+    for index in range(2):
+        geometry['translation_%d' % index][dut] = 0.
+    for i in range(3):
+        for j in range(3):
+            if i == j:
+                geometry['rotation_%d_%d' % (i, j)][dut] = 1.
+            else:
+                geometry['rotation_%d_%d' % (i, j)][dut] = 0.
