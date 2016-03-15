0041d6d77562d6a4904fa8fdb128981e8970fde9
DavidLP <david-leon.pohl@rub.de>
2016-03-15 09:53:30 +0100

    MAINT: formatting

--- examples/eutelescope_example_fine_alignment.py
+++ examples/eutelescope_example_fine_alignment.py
@@ -1,6 +1,5 @@
 '''Example script to run a full analysis on telescope data. The original data can be found in the example folder of the EuTelescope framework.
-The telescope consists of 6 planes with 15 cm clearance between the planes. The residual for the second plane (DUT 1) is about 8 um and comparable
-to the residuals from EuTelescope software (6 um).
+The telescope consists of 6 planes with 15 cm clearance between the planes.
 
 The Mimosa26 has an active area of 21.2mm x 10.6mm and the pixel matrix consists of 1152 columns and 576 rows (18.4um x 18.4um pixel size).
 The total size of the chip is 21.5mm x 13.7mm x 0.036mm (radiation length 9.3660734)
@@ -35,7 +34,7 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
     dut_names = ("Tel_0", "Tel_1", "Tel_2", "Tel_3", "Tel_4", "Tel_5")
 
     output_folder = os.path.split(data_files[0])[0]  # define a folder where all output data and plots are stored
-    
+
     geo_file = os.path.join(output_folder, 'MimosaGeometry.h5')
 
     # The following shows a complete test beam analysis by calling the seperate function in correct order
@@ -48,7 +47,7 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
     pool = Pool()
     multiple_results = [pool.apply_async(hit_analysis.remove_noisy_pixels, kwds=kwarg) for kwarg in kwargs]
     noisy_pixels_files = [res.get() for res in multiple_results]
-  
+
     # Cluster hits off all DUTs
     kwargs = [{
         'input_hits_file': noisy_pixels_files[i],
@@ -62,7 +61,7 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
     pool.close()
     pool.join()
     noisy_pixels_cluster_files = [res.get() for res in multiple_results]
-    
+
     noisy_pixels_files = [os.path.splitext(data_files[i])[0] + '_noisy_pixels.h5' for i in range(len(data_files))]
     noisy_pixels_cluster_files = [os.path.splitext(data_files[i])[0] + '_noisy_pixels_cluster.h5' for i in range(len(data_files))]
 
@@ -72,7 +71,7 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
                                  n_pixels=n_pixels,
                                  pixel_size=pixel_size,
                                  dut_names=dut_names)
- 
+
     # Create alignment data for the DUT positions to the first DUT from the correlation data
     # When needed, set offset and error cut for each DUT as list of tuples
     dut_alignment.coarse_alignment(input_correlation_file=os.path.join(output_folder, 'Correlation.h5'),
@@ -80,19 +79,19 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
                                    output_pdf_file=os.path.join(output_folder, 'Alignment.pdf'),
                                    pixel_size=pixel_size,
                                    non_interactive=True,  # Tries to find cuts automatically; deactivate to do this manualy
-                                   fix_slope=True)  
- 
+                                   fix_slope=True)
+
     # Correct all DUT hits via alignment information and merge the cluster tables to one tracklets table aligned at the event number
     dut_alignment.merge_cluster_data(input_cluster_files=noisy_pixels_cluster_files,
                                      input_alignment_file=os.path.join(output_folder, 'Alignment.h5'),
                                      output_tracklets_file=os.path.join(output_folder, 'Tracklets.h5'),
                                      pixel_size=pixel_size)
-  
+
     # Find tracks from the tracklets and stores the with quality indicator into track candidates table
     track_analysis.find_tracks(input_tracklets_file=os.path.join(output_folder, 'Tracklets.h5'),
                                input_alignment_file=os.path.join(output_folder, 'Alignment.h5'),
                                output_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'))
-  
+
     # Fit the track candidates and create new track table
     track_analysis.fit_tracks(input_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'),
                               output_tracks_file=os.path.join(output_folder, 'Tracks.h5'),
@@ -102,48 +101,51 @@ if __name__ == '__main__':  # main entry point is needed for multiprocessing und
                               include_duts=[-1, 1],  # Use only the DUT before and after the actual DUT for track fitting / interpolation
                               ignore_duts=None,
                               track_quality=2)
-  
+
     # Calculate the residuals to check the alignment
     result_analysis.calculate_residuals(input_tracks_file=os.path.join(output_folder, 'Tracks.h5'),
                                         output_pdf=os.path.join(output_folder, 'Residuals.pdf'),
                                         z_positions=z_positions,
                                         use_duts=None,
                                         max_chi2=None)
-      
+
+    # Do a fine alignment utilizing tracks and residuals minimization
     dut_alignment.fine_alignment(input_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'),
-                         alignment_file=geo_file,
-                         z_positions=z_positions,
-                         output_pdf=os.path.join(output_folder, 'FineAlignment.pdf'),
-                         fit_duts = range(6),
-                         include_duts=[-5,-4,-3,-2,-1,1,2,3,4,5],
-                         create_new_geometry=True)
-    track_analysis.fit_tracks_kalman(input_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'), 
-                                     output_tracks_file=os.path.join(output_folder, 'Tracks_interpolation.h5'), 
-                                     geometry_file=geo_file, 
-                                     z_positions=z_positions, 
+                                 alignment_file=geo_file,
+                                 z_positions=z_positions,
+                                 output_pdf=os.path.join(output_folder, 'FineAlignment.pdf'),
+                                 fit_duts=range(6),
+                                 include_duts=[-5, -4, -3, -2, -1, 1, 2, 3, 4, 5],
+                                 create_new_geometry=True)
+
+    # Fit the tracks using a straight line fit
+    track_analysis.fit_tracks_kalman(input_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'),
+                                     output_tracks_file=os.path.join(output_folder, 'Tracks_interpolation.h5'),
+                                     geometry_file=geo_file,
+                                     z_positions=z_positions,
                                      fit_duts=[1, 2, 3, 4],  # Fit tracks for all DUTs
-                                     ignore_duts=None, 
-                                     include_duts=[-1, 1], 
-                                     track_quality=2, 
-                                     max_tracks=None, 
+                                     ignore_duts=None,
+                                     include_duts=[-1, 1],
+                                     track_quality=2,
+                                     max_tracks=None,
                                      output_pdf=os.path.join(output_folder, 'Tracks.pdf'),
-                                     use_correlated=False, 
-                                     method="Interpolation", 
-                                     pixel_size=pixel_size, 
-                                     chunk_size=10000)
-    
-    track_analysis.fit_tracks_kalman(input_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'), 
-                                     output_tracks_file=os.path.join(output_folder, 'Tracks_kalman.h5'), 
-                                     geometry_file=geo_file, 
-                                     z_positions=z_positions, 
-                                     fit_duts=[1, 2, 3, 4],  # Fit tracks for all DUTs
-                                     ignore_duts=None, 
-                                     include_duts=[-5, -4, -3, -2, -1, 1, 2, 3, 4, 5], 
-                                     track_quality=2, 
-                                     max_tracks=None, 
-                                     output_pdf=os.path.join(output_folder, 'Tracks.pdf'),
-                                     use_correlated=False, 
-                                     method="Kalman", 
-                                     pixel_size=pixel_size, 
+                                     use_correlated=False,
+                                     method="Interpolation",
+                                     pixel_size=pixel_size,
                                      chunk_size=10000)
 
+    # Fit the tracks using a kalman filter
+    track_analysis.fit_tracks_kalman(input_track_candidates_file=os.path.join(output_folder, 'TrackCandidates.h5'),
+                                     output_tracks_file=os.path.join(output_folder, 'Tracks_kalman.h5'),
+                                     geometry_file=geo_file,
+                                     z_positions=z_positions,
+                                     fit_duts=[1, 2, 3, 4],  # Fit tracks for all DUTs
+                                     ignore_duts=None,
+                                     include_duts=[-5, -4, -3, -2, -1, 1, 2, 3, 4, 5],
+                                     track_quality=2,
+                                     max_tracks=None,
+                                     output_pdf=os.path.join(output_folder, 'Tracks.pdf'),
+                                     use_correlated=False,
+                                     method="Kalman",
+                                     pixel_size=pixel_size,
+                                     chunk_size=10000)
