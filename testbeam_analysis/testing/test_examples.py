64039383d887703563f72adec04fae5af35a15da
DavidLP <david-leon.pohl@rub.de>
2016-04-21 07:57:53 +0200

    BUG: fix paths

--- testbeam_analysis/testing/test_examples.py
+++ testbeam_analysis/testing/test_examples.py
@@ -8,8 +8,8 @@ import testbeam_analysis
 from testbeam_analysis.tools import data_selection
 
 package_path = os.path.dirname(testbeam_analysis.__file__)  # Get the absoulte path of the online_monitor installation
-script_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(package_path)) + r'/examples'))
-tests_data_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(package_path)) + r'/examples/data'))
+script_folder = os.path.abspath(os.path.join(package_path, r'../examples/'))
+tests_data_folder = os.path.abspath(os.path.join(os.path.realpath(script_folder), r'data/'))
 
 
 def run_script_in_process(script, command=None):  # Run python script in blocking mode
@@ -31,13 +31,13 @@ class TestExamples(unittest.TestCase):
         cls.output_folder = tests_data_folder
 
         # Reduce the example data to make it possible to test the examples in CI environments
-        cls.examples_fei4_hit_files = [cls.output_folder + r'\TestBeamData_FEI4_DUT0.h5',
-                                       cls.output_folder + r'\TestBeamData_FEI4_DUT1.h5',
-                                       cls.output_folder + r'\TestBeamData_FEI4_DUT4.h5',
-                                       cls.output_folder + r'\TestBeamData_FEI4_DUT5.h5']
+        cls.examples_fei4_hit_files = [os.path.join(cls.output_folder, r'TestBeamData_FEI4_DUT0.h5'),
+                                       os.path.join(cls.output_folder, r'TestBeamData_FEI4_DUT1.h5'),
+                                       os.path.join(cls.output_folder, r'TestBeamData_FEI4_DUT4.h5'),
+                                       os.path.join(cls.output_folder, r'TestBeamData_FEI4_DUT5.h5')]
         data_selection.reduce_hit_files(cls.examples_fei4_hit_files, fraction=100)
 
-        cls.examples_mimosa_hit_files = [cls.output_folder + r'\TestBeamData_Mimosa26_DUT%d.h5' % i for i in range(6)]
+        cls.examples_mimosa_hit_files = [os.path.join(cls.output_folder, r'TestBeamData_Mimosa26_DUT%d.h5') % i for i in range(6)]
         data_selection.reduce_hit_files(cls.examples_mimosa_hit_files, fraction=100)
 
         # Remove old files and rename reduced files
@@ -49,15 +49,15 @@ class TestExamples(unittest.TestCase):
             os.rename(file_name[:-3] + '_reduced.h5', file_name)
     @unittest.SkipTest # FIXME:
     def test_mimosa_example(self):
-        return_value = run_script_in_process(script_folder + r'\eutelescope_example.py')
+        return_value = run_script_in_process(os.path.join(script_folder, r'eutelescope_example.py'))
         self.assertEqual(return_value, 0)
 
     def test_fei4_example(self):
-        return_value = run_script_in_process(script_folder + r'\fei4_telescope_example.py')
+        return_value = run_script_in_process(os.path.join(script_folder, r'fei4_telescope_example.py'))
         self.assertEqual(return_value, 0)
 
     def test_simulated_data_example(self):
-        return_value = run_script_in_process(script_folder + r'\simulated_data_example.py')
+        return_value = run_script_in_process(os.path.join(script_folder, r'simulated_data_example.py'))
         self.assertEqual(return_value, 0)
 
 
