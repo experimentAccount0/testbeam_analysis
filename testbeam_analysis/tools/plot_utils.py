e0de8654ad195d2c4260c81802b6eae9eb026566
DavidLP <david-leon.pohl@rub.de>
2016-07-13 09:49:16 +0200

    ENH: plot chi2 with better limits and log y

--- testbeam_analysis/tools/plot_utils.py
+++ testbeam_analysis/tools/plot_utils.py
@@ -593,10 +593,12 @@ def plot_events(input_tracks_file, event_range, dut=None, max_chi2=None, output_
 def plot_track_chi2(chi2s, fit_dut, output_fig):
     # Plot track chi2 and angular distribution
     plt.clf()
-    plt.hist(chi2s, bins=200)
+    limit_x = np.percentile(chi2s, q=68.3)  # Plot up to 1 sigma of the chi2 range
+    plt.hist(chi2s, bins=200, range=(0, limit_x))
     plt.grid()
     plt.xlabel('Track Chi2 [um*um]')
     plt.ylabel('#')
+    plt.yscale('log')
     plt.title('Track Chi2 for DUT %d tracks' % fit_dut)
     output_fig.savefig()
 
