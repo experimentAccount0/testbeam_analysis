5772bb552632579a5f363b44ed15ddb64513ce72
DavidLP <david-leon.pohl@rub.de>
2016-07-13 07:32:16 +0200

    ENH: test also negative angles

--- testbeam_analysis/testing/test_geometry_utils.py
+++ testbeam_analysis/testing/test_geometry_utils.py
@@ -20,9 +20,9 @@ class TestTrackAnalysis(unittest.TestCase):
             for x in range(-3, 4, 3):  # Loop over x translation values
                 for y in range(-3, 4, 3):  # Loop over y translation values
                     for z in range(-3, 4, 3):  # Loop over z translation values
-                        for alpha in [0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop x rotation values
-                            for beta in [0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop y rotation values
-                                for gamma in [0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop z rotation values
+                        for alpha in [- np.pi, -np.pi / 3., 0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop x rotation values
+                            for beta in [- np.pi, -np.pi / 3., 0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop y rotation values
+                                for gamma in [- np.pi, -np.pi / 3., 0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop z rotation values
                                     position_g = np.array([position[0], position[1], position[2], 1])  # Extend global position dimension
                                     transformation_matrix_to_local = geometry_utils.global_to_local_transformation_matrix(x, y, z, alpha, beta, gamma)
                                     transformation_matrix_to_global = geometry_utils.local_to_global_transformation_matrix(x, y, z, alpha, beta, gamma)
@@ -32,17 +32,17 @@ class TestTrackAnalysis(unittest.TestCase):
 
     def test_rotation_matrices(self):
         # Check that the rotation matrices in x, y, z have the features of a rotation matrix (det = 1, inverse = transposed matrix)
-        for alpha in [0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop x rotation values
+        for alpha in [- np.pi, -np.pi / 3., 0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop x rotation values
             rotation_matrix_x = geometry_utils.rotation_matrix_x(alpha)
             self.assertAlmostEqual(np.linalg.det(rotation_matrix_x), 1)
             self.assertTrue(np.allclose(rotation_matrix_x.T, np.linalg.inv(rotation_matrix_x)))
 
-        for beta in [0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop y rotation values
+        for beta in [- np.pi, -np.pi / 3., 0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop y rotation values
             rotation_matrix_y = geometry_utils.rotation_matrix_y(beta)
             self.assertAlmostEqual(np.linalg.det(rotation_matrix_y), 1)
             self.assertTrue(np.allclose(rotation_matrix_y.T, np.linalg.inv(rotation_matrix_y)))
 
-        for gamma in [0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop z rotation values
+        for gamma in [- np.pi, -np.pi / 3., 0, np.pi / 4., np.pi / 3., np.pi / 2., 3 * np.pi / 4., np.pi, 4. * np.pi / 3.]:  # Loop z rotation values
             rotation_matrix_z = geometry_utils.rotation_matrix_z(gamma)
             self.assertAlmostEqual(np.linalg.det(rotation_matrix_z), 1)
             self.assertTrue(np.allclose(rotation_matrix_z.T, np.linalg.inv(rotation_matrix_z)))
@@ -55,7 +55,7 @@ class TestTrackAnalysis(unittest.TestCase):
                     self.assertAlmostEqual(np.linalg.det(rotation_matrix), 1)
                     self.assertTrue(np.allclose(rotation_matrix.T, np.linalg.inv(rotation_matrix)))
 
-    def test_apply_transformation_matrix(self):
+    def test_apply_transformation_matrix(self):  # Test the apply transformation matrix function
         x, y, z = np.arange(10), np.arange(1, 11), np.arange(2, 12)
         # Test 1: Transformation matrix that is a translation by (1, 2, 3) without rotation
         transformation_matrix = geometry_utils.global_to_local_transformation_matrix(x=1,
