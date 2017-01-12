999ea628b61e205cb72e5c51ef7ff9e4d143c5b1
DavidLP <David-Leon.Pohl@rub.de>
2017-01-12 18:47:25 +0100

    DOC: deactivate rtds virtual environment packages that fail to build

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import sphinx_rtd_theme
 import mock
 import sys
 
-MOCK_MODULES = ['pybar_fei4_converter', 'pybar_mimosa26_converter']
+MOCK_MODULES = ['pybar_fei4_converter', 'pybar_mimosa26_converter', 'numba']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
