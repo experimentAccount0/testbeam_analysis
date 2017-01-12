5ff7e44069ad965e5f916f271cb4a5c3371d2a20
DavidLP <David-Leon.Pohl@rub.de>
2017-01-12 14:58:37 +0100

    MAINT

--- testbeam_analysis/examples/eutelescope.py
+++ testbeam_analysis/examples/eutelescope.py
@@ -68,132 +68,132 @@ def run_analysis():
     dut_names = ("Tel_0", "Tel_1", "Tel_2", "Tel_3", "Tel_4", "Tel_5")
 
     # Create output subfolder where all output data and plots are stored
-    output_folder = r'/media/documents/TB_test/output/'
-#     if not os.path.exists(output_folder):
-#         os.makedirs(output_folder)
+    output_folder = os.path.join(os.path.split(data_files[0])[0], 'output')
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     # The following shows a complete test beam analysis by calling the
     # seperate function in correct order
 
-#     # Remove hot pixels, only needed for devices wih noisy pixel like Mimosa 26
-#     # A pool of workers to remove the noisy pixels in all files in parallel
-#     kwargs = [{
-#         'input_hits_file': data_files[i],
-#         'n_pixel': n_pixels[i],
-#         'pixel_size': pixel_size[i],
-#         'threshold': 0.5,
-#         'dut_name': dut_names[i]} for i in range(0, len(data_files))]
-#     pool = Pool()
-#     for kwarg in kwargs:
-#         pool.apply_async(hit_analysis.remove_noisy_pixels, kwds=kwarg)
-#     pool.close()
-#     pool.join()
-# 
-#     # Cluster hits off all DUTs
-#     # A pool of workers to cluster hits in all files in parallel
-#     kwargs = [{
-#         'input_hits_file': data_files[i][:-3] + '_noisy_pixels.h5',
-#         'max_x_distance': 3,
-#         'max_y_distance': 3,
-#         'max_time_distance': 2,
-#         'max_cluster_hits': 1000000,
-#         'dut_name': dut_names[i]} for i in range(0, len(data_files))]
-#     pool = Pool()
-#     for kwarg in kwargs:
-#         pool.apply_async(hit_analysis.cluster_hits, kwds=kwarg)
-#     pool.close()
-#     pool.join()
-# 
-#     # Correlate the row / column of each DUT
-#     input_cluster_files = [data_file[:-3] + '_noisy_pixels_cluster.h5'
-#                            for data_file in data_files]
-#     dut_alignment.correlate_cluster(input_cluster_files=input_cluster_files,
-#                                     output_correlation_file=os.path.join(
-#                                         output_folder, 'Correlation.h5'),
-#                                     n_pixels=n_pixels,
-#                                     pixel_size=pixel_size,
-#                                     dut_names=dut_names)
+    # Remove hot pixels, only needed for devices wih noisy pixel like Mimosa 26
+    # A pool of workers to remove the noisy pixels in all files in parallel
+    kwargs = [{
+        'input_hits_file': data_files[i],
+        'n_pixel': n_pixels[i],
+        'pixel_size': pixel_size[i],
+        'threshold': 0.5,
+        'dut_name': dut_names[i]} for i in range(0, len(data_files))]
+    pool = Pool()
+    for kwarg in kwargs:
+        pool.apply_async(hit_analysis.remove_noisy_pixels, kwds=kwarg)
+    pool.close()
+    pool.join()
 
-#     # Create prealignment relative to the first DUT from the correlation data
-#     input_correlation_file = os.path.join(output_folder, 'Correlation.h5')
-#     dut_alignment.prealignment(input_correlation_file=input_correlation_file,
-#                                output_alignment_file=os.path.join(
-#                                    output_folder, 'Alignment.h5'),
-#                                z_positions=z_positions,
-#                                pixel_size=pixel_size,
-#                                dut_names=dut_names,
-#                                # This data has several tracks per event and
-#                                # noisy pixel, thus fit existing background
-#                                fit_background=True,
-#                                # Tries to find cuts automatically;
-#                                # deactivate to do this manualy
-#                                non_interactive=False)
-# 
-#     # Merge the cluster tables to one merged table aligned at the event number
-#     input_cluster_files = [data_file[:-3] + '_noisy_pixels_cluster.h5'
-#                            for data_file in data_files]
-#     dut_alignment.merge_cluster_data(input_cluster_files=input_cluster_files,
-#                                      output_merged_file=os.path.join(
-#                                          output_folder, 'Merged.h5'),
-#                                      n_pixels=n_pixels,
-#                                      pixel_size=pixel_size)
-# 
-#     # Apply the prealignment to the merged cluster table to create tracklets
-#     dut_alignment.apply_alignment(input_hit_file=os.path.join(
-#         output_folder, 'Merged.h5'),
-#         input_alignment=os.path.join(
-#         output_folder, 'Alignment.h5'),
-#         output_hit_aligned_file=os.path.join(
-#         output_folder, 'Tracklets_prealigned.h5'),
-#         force_prealignment=True)
-# 
-#     # Find tracks from the prealigned tracklets and stores them with quality
-#     # indicator into track candidates table
-#     track_analysis.find_tracks(input_tracklets_file=os.path.join(
-#         output_folder, 'Tracklets_prealigned.h5'),
-#         input_alignment_file=os.path.join(
-#         output_folder, 'Alignment.h5'),
-#         output_track_candidates_file=os.path.join(
-#         output_folder,
-#         'TrackCandidates_prealignment.h5')
-#     )
+    # Cluster hits off all DUTs
+    # A pool of workers to cluster hits in all files in parallel
+    kwargs = [{
+        'input_hits_file': data_files[i][:-3] + '_noisy_pixels.h5',
+        'max_x_distance': 3,
+        'max_y_distance': 3,
+        'max_time_distance': 2,
+        'max_cluster_hits': 1000000,
+        'dut_name': dut_names[i]} for i in range(0, len(data_files))]
+    pool = Pool()
+    for kwarg in kwargs:
+        pool.apply_async(hit_analysis.cluster_hits, kwds=kwarg)
+    pool.close()
+    pool.join()
+
+    # Correlate the row / column of each DUT
+    input_cluster_files = [data_file[:-3] + '_noisy_pixels_cluster.h5'
+                           for data_file in data_files]
+    dut_alignment.correlate_cluster(input_cluster_files=input_cluster_files,
+                                    output_correlation_file=os.path.join(
+                                        output_folder, 'Correlation.h5'),
+                                    n_pixels=n_pixels,
+                                    pixel_size=pixel_size,
+                                    dut_names=dut_names)
+
+    # Create prealignment relative to the first DUT from the correlation data
+    input_correlation_file = os.path.join(output_folder, 'Correlation.h5')
+    dut_alignment.prealignment(input_correlation_file=input_correlation_file,
+                               output_alignment_file=os.path.join(
+                                   output_folder, 'Alignment.h5'),
+                               z_positions=z_positions,
+                               pixel_size=pixel_size,
+                               dut_names=dut_names,
+                               # This data has several tracks per event and
+                               # noisy pixel, thus fit existing background
+                               fit_background=True,
+                               # Tries to find cuts automatically;
+                               # deactivate to do this manualy
+                               non_interactive=True)
+
+    # Merge the cluster tables to one merged table aligned at the event number
+    input_cluster_files = [data_file[:-3] + '_noisy_pixels_cluster.h5'
+                           for data_file in data_files]
+    dut_alignment.merge_cluster_data(input_cluster_files=input_cluster_files,
+                                     output_merged_file=os.path.join(
+                                         output_folder, 'Merged.h5'),
+                                     n_pixels=n_pixels,
+                                     pixel_size=pixel_size)
+
+    # Apply the prealignment to the merged cluster table to create tracklets
+    dut_alignment.apply_alignment(input_hit_file=os.path.join(
+        output_folder, 'Merged.h5'),
+        input_alignment=os.path.join(
+        output_folder, 'Alignment.h5'),
+        output_hit_aligned_file=os.path.join(
+        output_folder, 'Tracklets_prealigned.h5'),
+        force_prealignment=True)
+
+    # Find tracks from the prealigned tracklets and stores them with quality
+    # indicator into track candidates table
+    track_analysis.find_tracks(input_tracklets_file=os.path.join(
+        output_folder, 'Tracklets_prealigned.h5'),
+        input_alignment_file=os.path.join(
+        output_folder, 'Alignment.h5'),
+        output_track_candidates_file=os.path.join(
+        output_folder,
+        'TrackCandidates_prealignment.h5')
+    )
 
     # The following two steps are for demonstration only.
     # They show track fitting and residual calculation on
     # prealigned hits. Usually you are not interested in this and will use
     # the aligned hits directly.
 
-#     # Step 1.: Fit the track candidates and create new track table (using the
-#     # prealignment!)
-#     track_analysis.fit_tracks(input_track_candidates_file=os.path.join(
-#         output_folder,
-#         'TrackCandidates_prealignment.h5'),
-#         input_alignment_file=os.path.join(
-#         output_folder, 'Alignment.h5'),
-#         output_tracks_file=os.path.join(
-#         output_folder, 'Tracks_prealigned.h5'),
-#         # To get unconstrained residuals do not use DUT
-#         # hit for track fit
-#         exclude_dut_hit=True,
-#         # This is just for demonstration purpose, usually
-#         # uses fully aligned hits
-#         force_prealignment=True,
-#         selection_track_quality=0)  # We will cut on chi2
-# 
-#     # Step 2.:  Calculate the residuals to check the alignment (using the
-#     # prealignment!)
-#     result_analysis.calculate_residuals(input_tracks_file=os.path.join(
-#         output_folder, 'Tracks_prealigned.h5'),
-#         input_alignment_file=os.path.join(
-#         output_folder, 'Alignment.h5'),
-#         output_residuals_file=os.path.join(
-#         output_folder, 'Residuals_prealigned.h5'),
-#         n_pixels=n_pixels,
-#         pixel_size=pixel_size,
-#         max_chi2=2000,
-#         # This is just for demonstration purpose
-#         # you usually use fully aligned hits
-#         force_prealignment=True)
+    # Step 1.: Fit the track candidates and create new track table (using the
+    # prealignment!)
+    track_analysis.fit_tracks(input_track_candidates_file=os.path.join(
+        output_folder,
+        'TrackCandidates_prealignment.h5'),
+        input_alignment_file=os.path.join(
+        output_folder, 'Alignment.h5'),
+        output_tracks_file=os.path.join(
+        output_folder, 'Tracks_prealigned.h5'),
+        # To get unconstrained residuals do not use DUT
+        # hit for track fit
+        exclude_dut_hit=True,
+        # This is just for demonstration purpose, usually
+        # uses fully aligned hits
+        force_prealignment=True,
+        selection_track_quality=0)  # We will cut on chi2
+
+    # Step 2.:  Calculate the residuals to check the alignment (using the
+    # prealignment!)
+    result_analysis.calculate_residuals(input_tracks_file=os.path.join(
+        output_folder, 'Tracks_prealigned.h5'),
+        input_alignment_file=os.path.join(
+        output_folder, 'Alignment.h5'),
+        output_residuals_file=os.path.join(
+        output_folder, 'Residuals_prealigned.h5'),
+        n_pixels=n_pixels,
+        pixel_size=pixel_size,
+        max_chi2=2000,
+        # This is just for demonstration purpose
+        # you usually use fully aligned hits
+        force_prealignment=True)
 
     # Do an alignment step with the track candidates, corrects rotations and
     # is therefore much more precise than simple prealignment
