bf7d3cdb2eb7bd23d2bf65b3283f04d99d023cd0
Jens Janssen <janssen@physik.uni-bonn.de>
2017-01-24 11:14:05 +0100

    ENH: allow event number offset for the first file

--- testbeam_analysis/tools/data_selection.py
+++ testbeam_analysis/tools/data_selection.py
@@ -41,17 +41,20 @@ def combine_hit_files(hit_files, combined_file, event_number_offsets=None,
                                          filters=tb.Filters(complib='blosc',
                                                             complevel=5,
                                                             fletcher32=False))
-        event_number_offset = 0
         for index, hit_file in enumerate(hit_files):
+            if event_number_offsets and event_number_offsets[index]:
+                event_number_offset = event_number_offsets[index]
+            elif index == 0:
+                event_number_offset = 0  # by default no offset for the first file
+            else:
+                event_number_offset += last_event_number + 1  # increase by 1 to avoid duplicate numbers
+
             with tb.open_file(hit_file, mode='r') as in_file_h5:
                 for hits, _ in analysis_utils.data_aligned_at_events(
                         in_file_h5.root.Hits, chunk_size=chunk_size):
                     hits[:]['event_number'] += event_number_offset
                     hits_out.append(hits)
-                if not event_number_offsets:
-                    event_number_offset += (hits[-1]['event_number'] + 1)
-                else:
-                    event_number_offset = event_number_offsets[index]
+                last_event_number = hits[-1]['event_number']
                 used_event_number_offsets.append(event_number_offset)
 
     return used_event_number_offsets
