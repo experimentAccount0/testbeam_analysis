19691cae00c0724d657382cc6bf25e220709646c
DavidLP <David-Leon.Pohl@rub.de>
2017-01-27 13:53:19 +0100

    MAINT: formatting, coding

--- testbeam_analysis/gui/analysis_widgets.py
+++ testbeam_analysis/gui/analysis_widgets.py
@@ -1,4 +1,3 @@
-import sys
 import tables as tb
 
 # Qt imports
@@ -6,191 +5,191 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 
 
 class DataTable(QtWidgets.QTableWidget):
-    
+    ''' TODO: DOCSTRING MISSING'''
+
     def __init__(self):
-        
+
         QtWidgets.QTableWidget.__init__(self)
         self.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
         self.verticalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
         self.showGrid()
         self.setSortingEnabled(True)
-        
-    def moveDown(self):
-        
+
+    def move_down(self):
         ''' Move current row down one place '''
-        
         row = self.currentRow()
         column = self.currentColumn()
-        if row < self.rowCount()-1:
-            self.insertRow(row+2)
+        if row < self.rowCount() - 1:
+            self.insertRow(row + 2)
             for i in range(self.columnCount()):
-               self.setItem(row+2,i,self.takeItem(row,i))
-               self.setCurrentCell(row+2,column)
+                self.setItem(row + 2, i, self.takeItem(row, i))
+                self.setCurrentCell(row + 2, column)
             self.removeRow(row)
             self.setVerticalHeaderLabels(self.row_labels)
-                
-    def moveUp(self):
-        
-        ''' Move current row up one place '''  
-        
+
+    def move_up(self):
+        ''' Move current row up one place '''
         row = self.currentRow()
         column = self.currentColumn()
         if row > 0:
-            self.insertRow(row-1)
+            self.insertRow(row - 1)
             for i in range(self.columnCount()):
-               self.setItem(row-1,i,self.takeItem(row+1,i))
-               self.setCurrentCell(row-1,column)
-            self.removeRow(row+1)
+                self.setItem(row - 1, i, self.takeItem(row + 1, i))
+                self.setCurrentCell(row - 1, column)
+            self.removeRow(row + 1)
             self.setVerticalHeaderLabels(self.row_labels)
-        
-    def setDUTNames(self, name='Tel'):
-        
-        ''' Set DUT names for further analysis; Tel + z-position by default; '''
-        
+
+    def set_dut_names(self, name='Tel'):
+        ''' Set DUT names for further analysis
+
+            Std. setting is Tel_i  and i is index
+        '''
         for i in range(self.rowCount()):
             dutItem = QtGui.QTableWidgetItem()
             dutItem.setTextAlignment(QtCore.Qt.AlignCenter)
             dutItem.setText(name + '_%d' % i)
-            self.setItem(i,1,dutItem)
-        
-    def updateDUTNames(self):
-        
+            self.setItem(i, 1, dutItem)
+
+    def update_dut_names(self):
         ''' Read list of DUT names from table and return tuple of names'''
-        
+
         new = []
-        
         try:
             for row in range(self.rowCount()):
-                new.append(self.item(row,1).text())
+                new.append(self.item(row, 1).text())
         except AttributeError:
-                return None
-                
+            return None
+
         return tuple(new)
-        
-    def getData(self, table_widget, input_files):
-        
-        ''' Browse through file manager and select files; only allow HDF5 format '''
-        
-        for path in QtGui.QFileDialog.getOpenFileNames(self, 'Select data of DUTs', '~/', '*.h5')[0]:
+
+    def get_data(self, table_widget, input_files):
+        ''' Open file dialog and select data files
+
+            Only *.h5 files are allowed '''
+
+        caption = 'Select data of DUTs'
+        for path in QtGui.QFileDialog.getOpenFileNames(self,
+                                                       caption=caption,
+                                                       directory='~/',
+                                                       filter='*.h5')[0]:
             input_files.append(path)
-            
-        self.handleData(table_widget, input_files)
-        
-    def handleData(self, table_widget, input_files):
-        
+
+        self.handle_data(table_widget, input_files)
+
+    def handle_data(self, table_widget, input_files):
         ''' Arranges input_data in the table and re-news table if DUT amount/order has been updated '''
-        
+
         for widget in table_widget.children():
             if isinstance(widget, QtGui.QTableWidget):
                 table_widget.layout().removeWidget(widget)
-                
-        self.row_labels = [('DUT ' + '%d' % i) for i in range(len(input_files))]
+
+        self.row_labels = [('DUT ' + '%d' % i)
+                           for i in range(len(input_files))]
         self.column_labels = ['Path', 'DUT names', 'Status']
         self.setColumnCount(len(self.column_labels))
         self.setRowCount(len(self.row_labels))
         self.setHorizontalHeaderLabels(self.column_labels)
         self.setVerticalHeaderLabels(self.row_labels)
-        
+
         for i, dut in enumerate(input_files):
             pathItem = QtGui.QTableWidgetItem()
-            pathItem.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+            pathItem.setFlags(
+                QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             pathItem.setTextAlignment(QtCore.Qt.AlignLeft)
             pathItem.setText(dut)
-            self.setItem(i,0,pathItem)
-            
-        self.checkData(input_files)
-            
-    def checkData(self, input_files):
-        
-        ''' Checks if given input_files contain the necessary information like event_number, column, row, etc.; visualzes broken input '''
-        
-        data_requirements = ('event_number', 'frame', 'column', 'row', 'charge')
-        incompatible_data = {} # store indices and status of incompatible data
-        
+            self.setItem(i, 0, pathItem)
+
+        self.check_data(input_files)
+
+    def check_data(self, input_files):
+        ''' Checks if given input_files contain the necessary information like
+        event_number, column, row, etc.; visualzes broken input '''
+
+        field_req = ('event_number', 'frame', 'column', 'row', 'charge')
+        incompatible_data = {}  # store indices and status of incompatible data
+
         for i, path in enumerate(input_files):
-            with tb.open_file(path, mode='r') as f:
+            with tb.open_file(path, mode='r') as in_file:
                 try:
-                    if sorted(tuple(f.root.Hits.colnames)) == sorted(data_requirements):
-                        pass
-                    else:
-                        incompatible_data[i] = 'Data does not contain all required information!'
+                    if not all(name in field_req for name
+                               in in_file.root.Hits.colnames):
+                        incompatible_data[i] = 'Data does not contain all ' \
+                                                'required information!'
                 except tb.exceptions.NoSuchNodeError:
-                    incompatible_data[i] = 'NoSuchNodeError: Data does not contain hits!'
+                    incompatible_data[i] = 'NoSuchNodeError: Data does not ' \
+                                            'contain hits!'
 
         font = QtGui.QFont()
         font.setBold(True)
         font.setUnderline(True)
-        
+
         for row in range(self.rowCount()):
-            
             statusItem = QtGui.QTableWidgetItem()
             statusItem.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             statusItem.setTextAlignment(QtCore.Qt.AlignCenter)
-            
+
             if row in incompatible_data.keys():
                 statusItem.setText(incompatible_data[row])
-                self.setItem(row,2,statusItem)
-                
+                self.setItem(row, 2, statusItem)
+
                 for col in range(self.columnCount()):
                     try:
-                        self.item(row,col).setFont(font)
-                        self.item(row,col).setForeground(QtGui.QColor('red'))
+                        self.item(row, col).setFont(font)
+                        self.item(row, col).setForeground(QtGui.QColor('red'))
                     except AttributeError:
                         pass
             else:
                 statusItem.setText('Okay')
-                self.setItem(row,2,statusItem)
-            
-        
-    def updateData(self):
-        
-        ''' Updates the data/DUT content/order by re-reading the filespaths from the table and returnig a list with the new DUT order '''
-        
+                self.setItem(row, 2, statusItem)
+
+    def update_data(self):
+        ''' Updates the data/DUT content/order by re-reading the filespaths
+        from the table and returnig a list with the new DUT order '''
+
         new = []
         for row in range(self.rowCount()):
-            new.append(self.item(row,0).text())
+            new.append(self.item(row, 0).text())
         return new
-        
-    def removeData(self, table_widget):
-        
+
+    def remove_data(self):
         ''' Removes an entire row from the data table e.g. a DUT '''
-        
+
         row = self.currentRow()
         self.removeRow(row)
-        
-        
+
+
 class DropArea(QtWidgets.QFrame):
-    
+
     #fileDrop = QtCore.pyqtSignal()
-    
+
     def __init__(self):
-        
+
         QtWidgets.QFrame.__init__(self)
         self.setFrameStyle(QtGui.QFrame.StyledPanel | QtGui.QFrame.Sunken)
         self.setFrameShadow(QtGui.QFrame.Sunken)
         self.setLineWidth(3)
-        self.setMinimumSize(200,100)
-        self.setMaximumSize(300,175)
+        self.setMinimumSize(200, 100)
+        self.setMaximumSize(300, 175)
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.setAlignment(QtCore.Qt.AlignCenter)
         self.setAcceptDrops(True)
         self.input_files = []
         self.init_UI()
-        
+
     def init_UI(self):
-        
+
         drop_label = QtGui.QLabel('Drag and Drop')
         self.layout.addWidget(drop_label)
-        
+
     def dragEnterEvent(self, e):
 
         if e.mimeData().hasUrls:
             e.accept()
         else:
             e.ignore()
-            
+
     def dropEvent(self, e):
-        
+
         if e.mimeData().hasUrls:
             e.setDropAction(QtCore.Qt.CopyAction)
             e.accept()
@@ -200,12 +199,9 @@ class DropArea(QtWidgets.QFrame):
 
         else:
             e.ignore()
-            
-        #if len(self.input_files) != 0:
-            #self.fileDrop.emit()
-            
-    #def returnData(self):
-        #return self.input_files
-        
-        
-        
+
+        # if len(self.input_files) != 0:
+            # self.fileDrop.emit()
+
+    # def returnData(self):
+        # return self.input_files
--- testbeam_analysis/gui/main.py
+++ testbeam_analysis/gui/main.py
@@ -1,65 +1,70 @@
 import sys
 
-# Qt imports
 from PyQt5 import QtCore, QtWidgets, QtGui
 from pyqtgraph.dockarea import DockArea, Dock
 
-from testbeam_analysis import (hit_analysis, dut_alignment, track_analysis, result_analysis)
 from analysis_widgets import DataTable, DropArea
 
 PROJECT_NAME = 'Testbeam Analysis'
 
+
 class AnalysisWindow(QtWidgets.QMainWindow):
-    
-    def __init__(self):
-        
-        QtWidgets.QMainWindow.__init__(self)
+
+    def __init__(self, parent=None):
+        ''' TODO: DOCSTRING'''
+        super(AnalysisWindow, self).__init__(parent)
+
         self.setWindowTitle(PROJECT_NAME)
+
         self.screen = QtWidgets.QDesktopWidget().screenGeometry()
-        self.resize(0.75*self.screen.width(), 0.75*self.screen.height())
+        self.resize(0.75 * self.screen.width(), 0.75 * self.screen.height())
+
         self.init_UI()
         self.init_setup()
         self.file_tab()
-        
+
     def init_UI(self):
-        
+        ''' TODO: DOCSTRING'''
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.menu()
-        
-        # add tabs and widgets for the different analysis steps
-        self.tab_order = ('Files', 'Setup', 'HotPixelRemoval', 'Clustering', 'Alignment', 'TrackAnalysis')
+
+        # Add tabs and widgets for the different analysis steps
+        self.tab_order = ('Files', 'Setup', 'HotPixelRemoval', 'Clustering',
+                          'Alignment', 'TrackAnalysis')
         self.tab_widgets = {}
-                
-        # add QTabWidget for tabs
+
+        # Add QTabWidget for tabs
         tabs = QtWidgets.QTabWidget()
         self.setCentralWidget(tabs)
-        
-        # add DockArea to each tab
+
+        # Add DockArea to each tab
         for step in self.tab_order:
             self.tab_widgets[step] = DockArea()
             tabs.addTab(self.tab_widgets[step], step)
 
-        self.statusBar().showMessage("Hello and welcome to a simple and easy to use testbeam analysis!", 4000)
-        
+        self.statusBar().showMessage(
+            "Hello and welcome to a simple and easy to use testbeam analysis!",
+            4000)
+
     def init_setup(self):
-        
+
         self.input_files = list()
         self.output_folder = str()
         self.z_positions = 0
         self.dut_names = 0
         self.pixel_size = 0
         self.n_pixels = 0
-        
+
     def update_setup(self):
 
-        self.input_files = self.data_table.updateData()
-        self.dut_names = self.data_table.updateDUTNames()
-        
+        self.input_files = self.data_table.update_data()
+        self.dut_names = self.data_table.update_dut_names()
+
     def file_tab(self):
-        
-        # setup widgets
-        self.button_size = QtCore.QSize(150,40)
-        
+
+        # Setup widgets
+        self.button_size = QtCore.QSize(150, 40)
+
         self.select_widget = QtWidgets.QWidget()
         self.select_layout = QtWidgets.QGridLayout(self.select_widget)
         select_button = QtGui.QPushButton('Select data of DUTs')
@@ -67,10 +72,10 @@ class AnalysisWindow(QtWidgets.QMainWindow):
         dut_name_button = QtGui.QPushButton('Set DUT names')
         dut_name_button.setMaximumSize(self.button_size)
         da = DropArea()
-        self.select_layout.addWidget(da, 0, 0, 1 ,1)
-        self.select_layout.addWidget(select_button, 0, 1, 1 ,1)
-        self.select_layout.addWidget(dut_name_button, 0, 2, 1 ,1)
-        
+        self.select_layout.addWidget(da, 0, 0, 1, 1)
+        self.select_layout.addWidget(select_button, 0, 1, 1, 1)
+        self.select_layout.addWidget(dut_name_button, 0, 2, 1, 1)
+
         self.handle_widget = QtWidgets.QWidget()
         self.handle_layout = QtWidgets.QGridLayout(self.handle_widget)
         up_button = QtGui.QPushButton('Move up')
@@ -79,27 +84,46 @@ class AnalysisWindow(QtWidgets.QMainWindow):
         down_button.setMaximumSize(self.button_size)
         remove_button = QtGui.QPushButton('Remove DUT')
         remove_button.setMaximumSize(self.button_size)
-        self.handle_layout.addWidget(up_button, 0, 0, 1 ,1)
-        self.handle_layout.addWidget(down_button, 0, 1, 1 ,1)
-        self.handle_layout.addWidget(remove_button, 0, 2, 1 ,1)
-        
+        self.handle_layout.addWidget(up_button, 0, 0, 1, 1)
+        self.handle_layout.addWidget(down_button, 0, 1, 1, 1)
+        self.handle_layout.addWidget(remove_button, 0, 2, 1, 1)
+
         self.table_widget = QtWidgets.QWidget()
         self.table_layout = QtWidgets.QVBoxLayout(self.table_widget)
         self.data_table = DataTable()
         self.table_layout.addWidget(self.data_table)
-        
-        # connect buttons
-        #da.fileDrop.connect(self.data_table.getData(self.table_widget, da.returnData()))
-        select_button.clicked.connect(lambda: self.data_table.getData(self.table_widget, self.input_files))
-        [up_button.clicked.connect(x) for x in [lambda: self.data_table.moveUp(), lambda: self.update_setup(), lambda: self.data_table.checkData(self.input_files)]]
-        [down_button.clicked.connect(x) for x in [lambda: self.data_table.moveDown(), lambda: self.update_setup(), lambda: self.data_table.checkData(self.input_files)]]
-        [remove_button.clicked.connect(x) for x in [lambda: self.data_table.removeData(self.table_widget), lambda: self.update_setup(), lambda: self.data_table.handleData(self.table_widget, self.input_files)]]
-        [dut_name_button.clicked.connect(x) for x in [lambda: self.data_table.setDUTNames(), lambda: self.update_setup(), lambda: self.data_table.checkData(self.input_files)]]
-    
+
+        # Connect buttons
+        # da.fileDrop.connect(self.data_table.get_data(self.table_widget, da.returnData()))
+        select_button.clicked.connect(
+            lambda: self.data_table.get_data(self.table_widget,
+                                             self.input_files))
+
+        for x in [lambda: self.data_table.move_up(),
+                  lambda: self.update_setup(),
+                  lambda: self.data_table.check_data(self.input_files)]:
+            up_button.clicked.connect(x)
+
+        for x in [lambda: self.data_table.move_down(),
+                  lambda: self.update_setup(),
+                  lambda: self.data_table.check_data(self.input_files)]:
+            down_button.clicked.connect(x)
+
+        for x in [lambda: self.data_table.remove_data(),
+                  lambda: self.update_setup(),
+                  lambda: self.data_table.handle_data(self.table_widget,
+                                                      self.input_files)]:
+            remove_button.clicked.connect(x)
+
+        for x in [lambda: self.data_table.set_dut_names(),
+                  lambda: self.update_setup(),
+                  lambda: self.data_table.check_data(self.input_files)]:
+            dut_name_button.clicked.connect(x)
+
         select_dock = Dock('Select DUTs')
-        select_dock.setMaximumSize(self.screen.width()/2, 150)
+        select_dock.setMaximumSize(self.screen.width() / 2, 150)
         handle_dock = Dock('Handle selected \n DUT')
-        handle_dock.setMaximumSize(self.screen.width()/2, 150)
+        handle_dock.setMaximumSize(self.screen.width() / 2, 150)
         table_dock = Dock('Selected DUTs')
         select_dock.addWidget(self.select_widget)
         handle_dock.addWidget(self.handle_widget)
@@ -107,11 +131,10 @@ class AnalysisWindow(QtWidgets.QMainWindow):
         self.tab_widgets[self.tab_order[0]].addDock(select_dock, 'left')
         self.tab_widgets[self.tab_order[0]].addDock(handle_dock, 'right')
         self.tab_widgets[self.tab_order[0]].addDock(table_dock, 'bottom')
-        
-        
+
     def menu(self):
         self.file_menu = QtWidgets.QMenu('&File', self)
-        self.file_menu.addAction('&Quit', self.fileQuit,
+        self.file_menu.addAction('&Quit', self.file_quit,
                                  QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
         self.menuBar().addMenu(self.file_menu)
 
@@ -119,17 +142,16 @@ class AnalysisWindow(QtWidgets.QMainWindow):
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
 
-        #self.help_menu.addAction('&About', self.about)
+        # self.help_menu.addAction('&About', self.about)
 
-    def fileQuit(self):
+    def file_quit(self):
         self.close()
 
     def closeEvent(self, _):
-        self.fileQuit()
+        self.file_quit()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     aw = AnalysisWindow()
     aw.show()
     sys.exit(app.exec_())
-    
