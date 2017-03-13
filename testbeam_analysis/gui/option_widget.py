ada5323ec3215c4fc2e0a1de99600d0ad36a1afe
davidlp <pohl@physik.uni-bonn.de>
2017-03-13 22:07:58 +0100

    ENH: draft of iterable option widget

--- testbeam_analysis/gui/option_widget.py
+++ testbeam_analysis/gui/option_widget.py
@@ -174,3 +174,81 @@ class OptionBool(QtWidgets.QWidget):  # FIXME: optional not implemented
         if default_value is not None:
             rb_t.setChecked(default_value is True)
             rb_f.setChecked(default_value is False)
+
+
+class OptionMultiSlider(QtWidgets.QWidget):  # FIXME: steps size != 1 not supported
+    ''' Option sliders for several numbers
+
+        Shows the value as text and can increase range
+    '''
+
+    valueChanged = QtCore.pyqtSignal([float])
+
+    def __init__(self, name, n_values, default_value, optional, tooltip, parent=None):
+        super(OptionMultiSlider, self).__init__(parent)
+
+        # Slider with textbox to the right
+        layout_2 = QtWidgets.QHBoxLayout()
+        slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.edit = QtWidgets.QLineEdit()
+        validator = QtGui.QDoubleValidator()
+        self.edit.setValidator(validator)
+        self.edit.setMaxLength(3)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                            QtWidgets.QSizePolicy.Preferred)
+        self.edit.setSizePolicy(size_policy)
+        layout_2.addWidget(slider)
+        layout_2.addWidget(self.edit)
+
+        # Option name with slider below
+        layout = QtWidgets.QVBoxLayout(self)
+        text = QtWidgets.QLabel(name)
+        if tooltip:
+            text.setToolTip(tooltip)
+        if optional:
+            layout_1 = QtWidgets.QHBoxLayout()
+            layout_1.addWidget(text)
+            layout_1.addStretch(0)
+            check_box = QtWidgets.QCheckBox()
+            layout_1.addWidget(check_box)
+            layout.addLayout(layout_1)
+
+            check_box.stateChanged.connect(
+                lambda v: self._set_readonly(v == 0))
+            self._set_readonly()
+        else:
+            layout.addWidget(text)
+        layout.addLayout(layout_2)
+
+        slider.valueChanged.connect(lambda v: self.edit.setText(str(v)))
+        self.edit.returnPressed.connect(
+            lambda: slider.setMaximum(float(self.edit.text()) * 2))
+        slider.valueChanged.connect(lambda _: self._emit_value())
+
+        if default_value is not None:
+            slider.setValue(default_value)
+            # Needed because set value does not issue a value changed
+            # if value stays constant
+            self.edit.setText(str(slider.value()))
+
+    def _set_readonly(self, value=True):
+        if value:
+            palette = QtGui.QPalette()
+            palette.setColor(QtGui.QPalette.Base, QtCore.Qt.gray)
+            palette.setColor(QtGui.QPalette.Text, QtCore.Qt.darkGray)
+            self.edit.setPalette(palette)
+            self.edit.setReadOnly(True)
+            self._emit_value()
+        else:
+            palette = QtGui.QPalette()
+            palette.setColor(QtGui.QPalette.Base, QtCore.Qt.white)
+            palette.setColor(QtGui.QPalette.Text, QtCore.Qt.black)
+            self.edit.setPalette(palette)
+            self.edit.setReadOnly(False)
+            self._emit_value()
+
+    def _emit_value(self):
+        if self.edit.isReadOnly() or not self.edit.text():
+            self.valueChanged.emit(float('nan'))
+        else:
+            self.valueChanged.emit(int(self.edit.text()))
