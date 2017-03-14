dff4997dca340966b1bc1641addea54d000a37e2
DavidLP <David-Leon.Pohl@rub.de>
2017-03-14 17:45:25 +0100

    ENH: multi slider working

--- testbeam_analysis/gui/option_widget.py
+++ testbeam_analysis/gui/option_widget.py
@@ -4,6 +4,8 @@
     Optional options can be deactivated with the value None.
 '''
 
+import collections
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 
@@ -22,6 +24,7 @@ class OptionSlider(QtWidgets.QWidget):  # FIXME: steps size != 1 not supported
         layout_2 = QtWidgets.QHBoxLayout()
         slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.edit = QtWidgets.QLineEdit()
+        self.edit.setAlignment(QtCore.Qt.AlignCenter)
         validator = QtGui.QDoubleValidator()
         self.edit.setValidator(validator)
         self.edit.setMaxLength(3)
@@ -52,11 +55,13 @@ class OptionSlider(QtWidgets.QWidget):  # FIXME: steps size != 1 not supported
         layout.addLayout(layout_2)
 
         slider.valueChanged.connect(lambda v: self.edit.setText(str(v)))
-        self.edit.returnPressed.connect(
-            lambda: slider.setMaximum(float(self.edit.text()) * 2))
         slider.valueChanged.connect(lambda _: self._emit_value())
+        self.edit.returnPressed.connect(
+            lambda: slider.setMaximum(max((float(self.edit.text()) * 2), 1)))
+        self.edit.returnPressed.connect(lambda: slider.setValue(float(self.edit.text())))
 
         if default_value is not None:
+            slider.setMaximum(max((default_value * 2, 1)))
             slider.setValue(default_value)
             # Needed because set value does not issue a value changed
             # if value stays constant
@@ -94,6 +99,7 @@ class OptionText(QtWidgets.QWidget):
     def __init__(self, name, default_value, optional, tooltip=None, parent=None):
         super(OptionText, self).__init__(parent)
         self.edit = QtWidgets.QLineEdit()
+        self.edit.setAlignment(QtCore.Qt.AlignCenter)
         layout = QtWidgets.QVBoxLayout(self)
 
         text = QtWidgets.QLabel(name)
@@ -176,79 +182,103 @@ class OptionBool(QtWidgets.QWidget):  # FIXME: optional not implemented
             rb_f.setChecked(default_value is False)
 
 
-class OptionMultiSlider(QtWidgets.QWidget):  # FIXME: steps size != 1 not supported
+# FIXME: steps size != 1 not supported
+class OptionMultiSlider(QtWidgets.QWidget):
     ''' Option sliders for several numbers
 
         Shows the value as text and can increase range
     '''
 
-    valueChanged = QtCore.pyqtSignal([float])
+    valueChanged = QtCore.pyqtSignal(list)
 
-    def __init__(self, name, n_values, default_value, optional, tooltip, parent=None):
+    def __init__(self, name, labels, default_value, optional, tooltip, parent=None):
         super(OptionMultiSlider, self).__init__(parent)
+        # Check default value
+        if default_value is None:  # None is only supported for all values
+            default_value = 0.
+        if not isinstance(default_value, collections.Iterable):
+            default_value = [default_value] * len(labels)
+        if len(labels) != len(default_value):
+            raise ValueError('Number of default values does not match number of parameters')
 
-        # Slider with textbox to the right
-        layout_2 = QtWidgets.QHBoxLayout()
-        slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.edit = QtWidgets.QLineEdit()
-        validator = QtGui.QDoubleValidator()
-        self.edit.setValidator(validator)
-        self.edit.setMaxLength(3)
-        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                            QtWidgets.QSizePolicy.Preferred)
-        self.edit.setSizePolicy(size_policy)
-        layout_2.addWidget(slider)
-        layout_2.addWidget(self.edit)
+        max_val = max((max(default_value) * 2, 1))
 
-        # Option name with slider below
+        # Option name with sliders below
         layout = QtWidgets.QVBoxLayout(self)
         text = QtWidgets.QLabel(name)
         if tooltip:
             text.setToolTip(tooltip)
-        if optional:
+        if optional:  # Values can be unset
             layout_1 = QtWidgets.QHBoxLayout()
             layout_1.addWidget(text)
             layout_1.addStretch(0)
             check_box = QtWidgets.QCheckBox()
             layout_1.addWidget(check_box)
             layout.addLayout(layout_1)
+        else:
+            layout.addWidget(text)
 
+        self.edits = []
+
+        for i, label in enumerate(labels):  # Create one slider per label
+            # Slider with textbox to the right
+            layout_label = QtWidgets.QHBoxLayout()
+            slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+            # Text edit
+            edit = QtWidgets.QLineEdit()
+            edit.setAlignment(QtCore.Qt.AlignCenter)
+            edit.setValidator(QtGui.QDoubleValidator())
+            edit.setMaxLength(3)
+            size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                                QtWidgets.QSizePolicy.Preferred)
+            edit.setSizePolicy(size_policy)
+            layout_label.addWidget(QtWidgets.QLabel('  ' + label))
+            layout_label.addWidget(slider)
+            layout_label.addWidget(edit)
+
+            # Crazy shit: lambda late binding has to be prevented here
+            # http://docs.python-guide.org/en/latest/writing/gotchas/
+            slider.valueChanged.connect(lambda v, edit=edit: edit.setText(str(v)))
+            slider.valueChanged.connect(lambda _: self._emit_value())
+            edit.returnPressed.connect(lambda slider=slider, edit=edit: slider.setMaximum(max(float(edit.text()) * 2, 1)))
+            edit.returnPressed.connect(lambda slider=slider, edit=edit: slider.setValue(float(edit.text())))
+
+            slider.setMaximum(max_val)
+            slider.setValue(default_value[i])
+            # Needed because set value does not issue a value changed
+            # if value stays constant
+            edit.setText(str(slider.value()))
+
+            self.edits.append(edit)
+
+            layout.addLayout(layout_label)
+
+        if optional:
             check_box.stateChanged.connect(
                 lambda v: self._set_readonly(v == 0))
             self._set_readonly()
-        else:
-            layout.addWidget(text)
-        layout.addLayout(layout_2)
-
-        slider.valueChanged.connect(lambda v: self.edit.setText(str(v)))
-        self.edit.returnPressed.connect(
-            lambda: slider.setMaximum(float(self.edit.text()) * 2))
-        slider.valueChanged.connect(lambda _: self._emit_value())
-
-        if default_value is not None:
-            slider.setValue(default_value)
-            # Needed because set value does not issue a value changed
-            # if value stays constant
-            self.edit.setText(str(slider.value()))
 
     def _set_readonly(self, value=True):
         if value:
             palette = QtGui.QPalette()
             palette.setColor(QtGui.QPalette.Base, QtCore.Qt.gray)
             palette.setColor(QtGui.QPalette.Text, QtCore.Qt.darkGray)
-            self.edit.setPalette(palette)
-            self.edit.setReadOnly(True)
+            for edit in self.edits:
+                edit.setPalette(palette)
+                edit.setReadOnly(True)
             self._emit_value()
         else:
             palette = QtGui.QPalette()
             palette.setColor(QtGui.QPalette.Base, QtCore.Qt.white)
             palette.setColor(QtGui.QPalette.Text, QtCore.Qt.black)
-            self.edit.setPalette(palette)
-            self.edit.setReadOnly(False)
+            for edit in self.edits:
+                edit.setPalette(palette)
+                edit.setReadOnly(False)
             self._emit_value()
 
     def _emit_value(self):
-        if self.edit.isReadOnly() or not self.edit.text():
-            self.valueChanged.emit(float('nan'))
+        if not any([edit.isReadOnly() for edit in self.edits]):
+            values = [int(edit.text()) for edit in self.edits]
         else:
-            self.valueChanged.emit(int(self.edit.text()))
+            values = [None]
+        self.valueChanged.emit(values)
