1332177e853a2584eab7bfad89602d4da1bdd9b4
Jens Janssen <janssen@physik.uni-bonn.de>
2017-05-17 16:22:02 +0200

    MAINT: be more specific

--- testbeam_analysis/dut_alignment.py
+++ testbeam_analysis/dut_alignment.py
@@ -1096,6 +1096,7 @@ def _duts_alignment(track_candidates_file, alignment_file, align_duts, selection
             apply_alignment(input_hit_file=os.path.splitext(track_candidates_reduced)[0] + '_not_aligned.h5',
                             input_alignment_file=alignment_file,
                             output_hit_file=os.path.splitext(track_candidates_file)[0] + '_final_tmp_duts_%s.h5' % alignment_duts,
+                            force_prealignment=False,
                             chunk_size=chunk_size)
             fit_tracks(input_track_candidates_file=os.path.splitext(track_candidates_file)[0] + '_final_tmp_duts_%s.h5' % alignment_duts,
                        input_alignment_file=alignment_file,
@@ -1111,6 +1112,7 @@ def _duts_alignment(track_candidates_file, alignment_file, align_duts, selection
                                 output_residuals_file=os.path.splitext(track_candidates_file)[0] + '_residuals_final_tmp_duts_%s.h5' % alignment_duts,
                                 n_pixels=n_pixels,
                                 pixel_size=pixel_size,
+                                force_prealignment=False,
                                 use_fit_limits=use_fit_limits,
                                 plot=plot,
                                 chunk_size=chunk_size)
